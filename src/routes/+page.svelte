f7fc260b79db43f555294adc164e69933d7b7957
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-02 14:46:22 -0700

    Made landing page nav responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,46 @@
+<script>
+	import CloseMultiply from '$lib/assets/Close-Multiply.svelte';
+	import Menu from '$lib/assets/Menu.svelte';
+
+	let showMobileNav = false;
+</script>
+
 <header class="h-20 bg-grey-100 py-lg flex items-center px-lg">
 	<div class="flex items-center gap-md">
 		<div class="w-12 h-12 bg-grey-300 rounded-lg" />
 		<span class="text-md font-bold text-grey-800">Project Brew</span>
 	</div>
-	<nav class="flex items-center ml-auto gap-md">
+	<nav class="hidden items-center ml-auto gap-md md:flex">
 		<a href="/" class="font-bold text-grey-700">About</a>
-		<a href="/" class="font-bold text-grey-700 mr-md">GitHub</a>
+		<a href="/" class="font-bold text-grey-700 mr-md">Github</a>
 		<button class="button--secondary">Sign up</button>
 		<button class="button--primary">Sign in</button>
 	</nav>
+
+	<div class="block md:hidden items-center ml-auto">
+		<button
+			on:click={() => (showMobileNav = !showMobileNav)}
+			class="absolute right-lg top-6 z-50"
+			aria-controls="landing page navigation"
+			aria-expanded={showMobileNav}
+		>
+			<span class="sr-only">Menu</span>
+			{#if showMobileNav}
+				<CloseMultiply className="stroke-grey-700 h-8 w-8" />
+			{:else}
+				<Menu className="stroke-grey-700 h-8 w-8" />
+			{/if}
+		</button>
+		<nav
+			class="items-start justify-end ml-auto gap-md flex flex-col fixed top-0 right-0 bg-grey-100 h-screen w-7/12 p-6 
+			{showMobileNav ? 'translate-x-0' : 'translate-x-full'} transition-all duration-300 ease-out"
+		>
+			<a href="/" class="font-bold text-grey-700">About</a>
+			<a href="/" class="font-bold text-grey-700 mb-auto">Github</a>
+			<button class="button--secondary w-full">Sign up</button>
+			<button class="button--primary w-full">Sign in</button>
+		</nav>
+	</div>
 </header>
 
 <section class="text-center mb-md px-lg">
@@ -29,4 +61,3 @@
 		<button class="button--primary">Get Started Today</button>
 	</div>
 </section>
-
