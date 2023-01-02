d5afbd8b66f414df606c2b0671ece5d1150f8577
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-02 12:55:08 -0700

    Created desktop landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    import "../app.css"
+	import '../app.css';
 </script>
 
-<slot/>
+<main>
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,32 @@
-<script>
-	import Switch from '$lib/components/form/Switch.svelte';
-</script>
+<header class="h-20 bg-grey-100 py-lg flex items-center px-lg">
+	<div class="flex items-center gap-md">
+		<div class="w-12 h-12 bg-grey-300 rounded-lg" />
+		<span class="text-md font-bold text-grey-800">Project Brew</span>
+	</div>
+	<nav class="flex items-center ml-auto gap-md">
+		<a href="/" class="font-bold text-grey-700">About</a>
+		<a href="/" class="font-bold text-grey-700 mr-md">GitHub</a>
+		<button class="button--secondary">Sign up</button>
+		<button class="button--primary">Sign in</button>
+	</nav>
+</header>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="text-center mb-md px-lg">
+	<header>
+		<h1 class="text-lg font-bold text-grey-800 sm:text-2xl">Do project management right</h1>
+	</header>
+	<div class="flex flex-col gap-[10px] items-center text-grey-700 md:w-1/2 md:mx-auto">
+		<p>
+			With Project Brew it is incredibly easy to get started managing projects with your team or by
+			yourself.
+		</p>
+		<p>Do it on the go and at home.</p>
+		<p>Brew yourself a coffee and start managing projects <span class="underline">right!</span></p>
+		<p>
+			So, what are you waiting for! Get started for free and get up and running with you and you
+			team within minutes.
+		</p>
+		<button class="button--primary">Get Started Today</button>
+	</div>
+</section>
 
-<Switch id="input-test" text="Test" />
-<div class="" />
