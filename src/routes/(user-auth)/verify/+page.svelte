3a20fb01003b0566c90b182eb53d351298b8652f
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-03 10:55:08 -0700

    Created basic welcome page

--- src/routes/(user-auth)/verify/+page.svelte
+++ src/routes/(user-auth)/verify/+page.svelte
@@ -4,7 +4,9 @@
 
 <section class="grid place-content-center text-center h-screen">
 	<header>
-		<h1 class="text-xl md:text-2xl text-grey-900">Please verify your email address</h1>
+		<h1 class="text-xl md:text-2xl font-semibold text-grey-900">
+			Please verify your email address
+		</h1>
 	</header>
 	<div>
 		<p class="text-grey-700">
---
+++ src/routes/(user-auth)/welcome/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	const handleSetUserData = () => {};
+</script>
+
+<section
+	class="text-center mx-6 h-screen md:w-screen flex items-center justify-center flex-col gap-md"
+>
+	<header>
+		<h1 class="text-xl font-semibold text-grey-800 sm:text-2xl">Welcome to Project Brew!</h1>
+	</header>
+	<div class="flex flex-col items-center gap-md">
+		<p class="text-grey-700 md:w-1/2">
+			Please take a few moments to setup your account details. You can always change these in your
+			account settings.
+		</p>
+		<form on:submit|preventDefault={handleSetUserData}>
+			<input type="text" class="input--text mb-md" placeholder="Enter your name" />
+			<br />
+			<button class="button--primary">Set details</button>
+		</form>
+	</div>
+</section>
