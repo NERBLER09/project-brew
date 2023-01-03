66093dc1948b4edb0c899168ab539a4733d3339e
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-03 13:43:48 -0700

    Created main app layout file

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import { supabase } from '$lib/supabase';
+	import { onMount } from 'svelte';
+
+	const checkIfUserIsAuth = async (): Promise<boolean> => {
+		const { data: session } = await supabase.auth.getSession();
+		if (session.session) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	onMount(async () => {
+		if ((await checkIfUserIsAuth()) === false) {
+			goto('/');
+		}
+	});
+</script>
+
+<slot />
