35e47f0b69588d029695b139f55e574eb40c9178
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-03 13:13:47 -0700

    Set user data on welcome page

--- src/routes/(user-auth)/verify/+page.svelte
+++ src/routes/(user-auth)/verify/+page.svelte
@@ -2,14 +2,14 @@
 	import { signUpEmail } from '$lib/stores/user';
 </script>
 
-<section class="grid place-content-center text-center h-screen">
+<section class="mx-6 grid place-content-center text-center h-screen">
 	<header>
 		<h1 class="text-xl md:text-2xl font-semibold text-grey-900">
 			Please verify your email address
 		</h1>
 	</header>
 	<div>
-		<p class="text-grey-700">
+		<p class="text-grey-700 px-6 md:w-72">
 			An verification has been sent to {$signUpEmail}. Please verify your email address
 		</p>
 	</div>
--- src/routes/(user-auth)/welcome/+page.svelte
+++ src/routes/(user-auth)/welcome/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
-	const handleSetUserData = () => {};
+	import { supabase } from '$lib/supabase';
+
+	let name = '';
+
+	let error = '';
+
+	const handleSetUserData = async () => {
+		const {
+			data: { user }
+		} = await supabase.auth.getUser();
+		const id = user?.id;
+		const email = user?.email;
+
+		const { error: errorMsg } = await supabase.from('profiles').insert({
+			id,
+			email,
+			name
+		});
+
+		if (error) error = errorMsg?.message!;
+		else console.log('User data set! TODO: Route to main page');
+	};
 </script>
 
 <section
@@ -9,12 +30,17 @@
 		<h1 class="text-xl font-semibold text-grey-800 sm:text-2xl">Welcome to Project Brew!</h1>
 	</header>
 	<div class="flex flex-col items-center gap-md">
-		<p class="text-grey-700 md:w-1/2">
-			Please take a few moments to setup your account details. You can always change these in your
-			account settings.
+		<p class="text-grey-700 md:w-1/2 px-6">
+			Please take a few moments to setup your account details. You can always change and set these
+			in your account settings.
 		</p>
 		<form on:submit|preventDefault={handleSetUserData}>
-			<input type="text" class="input--text mb-md" placeholder="Enter your name" />
+			<input
+				type="text"
+				class="input--text mb-md"
+				placeholder="Enter your name"
+				bind:value={name}
+			/>
 			<br />
 			<button class="button--primary">Set details</button>
 		</form>
