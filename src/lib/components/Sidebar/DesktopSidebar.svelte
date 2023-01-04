56305b9d1284247eddba15675d7271babb70ec5c
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-03 18:17:51 -0700

    Updated desktop sidebar to better match mockup

--- src/lib/components/Sidebar/DesktopSidebar.svelte
+++ src/lib/components/Sidebar/DesktopSidebar.svelte
@@ -9,15 +9,24 @@
 	import Settings from '$lib/assets/Settings.svelte';
 	import User from '$lib/assets/User.svelte';
 	import Switch from '../form/Switch.svelte';
+
+	const handleSearch = () => {
+		console.log('Handle search');
+	};
 </script>
 
 <section class="hidden md:flex flex-col h-screen w-[16.625rem] bg-grey-100 px-md py-[1rem]">
-	<header class="flex items-center gap-md">
+	<header class="flex items-center gap-md ml-md">
 		<div class="w-xl h-xl bg-grey-300 rounded-lg" />
 		<span class="text-md font-semibold text-grey-800">Project Brew</span>
 	</header>
-	<form action="">
-		<div class="border border-grey-700  input--text flex items-center gap-md my-lg p-md">
+	<div
+		class="h-[2px] border-solid border border-grey-600 mt-lg w-11/12 rounded-full mx-auto drop-shadow-md"
+	/>
+	<form on:submit|preventDefault={handleSearch}>
+		<div
+			class="font-bold border-2 border-grey-600 border-solid box-border input--text flex items-center gap-md my-lg p-md placeholder:text-grey-700 text-grey-700"
+		>
 			<Search className="stroke-grey-700 w-6 h-6" />
 			<input type="text" placeholder="Search" class="bg-grey-200 border-none w-full p-0" />
 		</div>
@@ -70,28 +79,29 @@
 			Team
 		</a>
 	</nav>
-	<div class="mt-auto">
-		<nav class="flex flex-col gap-lg">
-			<a
-				href="#"
-				class="button--text items-start text-grey-700 hover:no-underline flex gap-md p-md w-full rounded hover:bg-gradient-to-r hover:from-grey-200 {$page.url.pathname.includes(
-					'settings'
-				)
-					? 'bg-grey-200'
-					: ''} "
-			>
-				<Settings className="stroke-grey-700 h-6 w-6" />
-				Settings
-			</a>
+	<div class="mt-auto flex flex-col gap-lg">
+		<a
+			href="#"
+			class="button--text items-start text-grey-700 hover:no-underline flex gap-md p-md w-full rounded hover:bg-gradient-to-r hover:from-grey-200 {$page.url.pathname.includes(
+				'settings'
+			)
+				? 'bg-grey-200'
+				: ''} "
+		>
+			<Settings className="stroke-grey-700 h-6 w-6" />
+			Settings
+		</a>
 
-			<button
-				class="button--text items-start text-grey-700 hover:no-underline flex gap-md p-md w-full rounded hover:bg-gradient-to-r hover:from-grey-200"
-			>
-				<LogOut className="stroke-grey-700 h-6 w-6" />
-				Log out
-			</button>
-		</nav>
-		<div class="mt-lg">
+		<button
+			class="button--text items-start text-grey-700 hover:no-underline flex gap-md p-md w-full rounded hover:bg-gradient-to-r hover:from-grey-200"
+		>
+			<LogOut className="stroke-grey-700 h-6 w-6" />
+			Log out
+		</button>
+		<div
+			class="h-[2px] border-solid border border-grey-600 w-11/12 rounded-full mx-auto drop-shadow-md"
+		/>
+		<div class="ml-md">
 			<Switch id="dark-mode" text="Dark Mode" />
 		</div>
 	</div>
