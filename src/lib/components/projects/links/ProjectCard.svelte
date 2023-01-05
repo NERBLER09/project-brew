dad0f44fdba629d5959d2b239748d80b109eafb7
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-05 12:03:38 -0700

    Created a basic project card component

---
+++ src/lib/components/projects/links/ProjectCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let id: string;
+	export let project_name: string;
+	export let banner: string = '';
+	export let description: string = '';
+	export let invited_people: string[] = [];
+	export let tags: string[] = [];
+</script>
+
+<section href="/app/projects/{id}" class="bg-grey-100 p-4 rounded-lg w-fit">
+	<header>
+		<h2 class="text-md font-medium text-grey-700">{project_name}</h2>
+	</header>
+	<div>
+		<p class="text-grey-700 text-sm font-medium h-[4ch] overflow-hidden text-ellipsis">
+			{description}
+		</p>
+
+		<div class="flex items-center gap-md flex-wrap pt-sm">
+			{#each tags as tag}
+				<div class="bg-grey-200 py-1 px-2 w-fit rounded">
+					<span class="text-grey-700 text-sm font-medium">{tag}</span>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
