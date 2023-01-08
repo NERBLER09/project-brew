2294c614d8a0b9c3d9207ffbe1573650127c9e65
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-08 11:31:45 -0700

    Show project cover when one is added on project card

--- src/lib/components/projects/links/ProjectCard.svelte
+++ src/lib/components/projects/links/ProjectCard.svelte
@@ -10,25 +10,38 @@
 <a href="/app/projects/{id}">
 	<section
 		href="/app/projects/{id}"
-		class="bg-grey-100 p-4 rounded-lg w-72 h-44 pt-[4.7rem] flex flex-col justify-end"
+		class="bg-grey-100 p-4 rounded-lg w-72 h-48 flex flex-col justify-end relative"
 	>
-		<header>
-			<span class="text-md font-medium text-grey-700">{project_name}</span>
-		</header>
-		<div>
-			<p
-				class="text-grey-700 text-sm font-medium h-[4ch] overflow-hidden text-ellipsis empty:hidden"
-			>
-				{description}
-			</p>
+		<div class="relative z-50">
+			<header>
+				<span class="text-md font-medium {banner ? 'text-grey-200' : 'text-grey-700'}"
+					>{project_name}</span
+				>
+			</header>
+			<div>
+				<p
+					class="{banner
+						? 'text-grey-200'
+						: 'text-grey-700'} text-sm font-medium h-[4ch] overflow-hidden text-ellipsis empty:hidden"
+				>
+					{description}
+				</p>
 
-			<div class="flex items-center gap-md flex-wrap pt-sm empty:hidden">
-				{#each tags as tag}
-					<div class="bg-grey-200 py-1 px-2 w-fit rounded">
-						<span class="text-grey-700 text-sm font-medium">{tag}</span>
-					</div>
-				{/each}
+				<div class="flex items-center gap-md flex-wrap pt-sm empty:hidden">
+					{#each tags as tag}
+						<div class="bg-grey-200 py-1 px-2 w-fit rounded">
+							<span class="text-grey-700 text-sm font-medium">{tag}</span>
+						</div>
+					{/each}
+				</div>
 			</div>
 		</div>
+		<img
+			src={banner}
+			alt="project banner"
+			class="absolute top-0 left-0 rounded-lg w-72 h-48 {banner
+				? 'block'
+				: 'hidden'} aspect-[2/3] object-cover object-center "
+		/>
 	</section>
 </a>
