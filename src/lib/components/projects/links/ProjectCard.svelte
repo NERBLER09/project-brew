16de3519885ea22a8453906e01bec13fddd93e66
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-08 09:09:55 -0700

    Created project page with header

--- src/lib/components/projects/links/ProjectCard.svelte
+++ src/lib/components/projects/links/ProjectCard.svelte
@@ -7,24 +7,28 @@
 	export let tags: string[] = [];
 </script>
 
-<section
-	href="/app/projects/{id}"
-	class="bg-grey-100 p-4 rounded-lg w-72 h-44 pt-[4.7rem] flex flex-col justify-end"
->
-	<header>
-		<span class="text-md font-medium text-grey-700">{project_name}</span>
-	</header>
-	<div>
-		<p class="text-grey-700 text-sm font-medium h-[4ch] overflow-hidden text-ellipsis empty:hidden">
-			{description}
-		</p>
+<a href="/app/projects/{id}">
+	<section
+		href="/app/projects/{id}"
+		class="bg-grey-100 p-4 rounded-lg w-72 h-44 pt-[4.7rem] flex flex-col justify-end"
+	>
+		<header>
+			<span class="text-md font-medium text-grey-700">{project_name}</span>
+		</header>
+		<div>
+			<p
+				class="text-grey-700 text-sm font-medium h-[4ch] overflow-hidden text-ellipsis empty:hidden"
+			>
+				{description}
+			</p>
 
-		<div class="flex items-center gap-md flex-wrap pt-sm empty:hidden">
-			{#each tags as tag}
-				<div class="bg-grey-200 py-1 px-2 w-fit rounded">
-					<span class="text-grey-700 text-sm font-medium">{tag}</span>
-				</div>
-			{/each}
+			<div class="flex items-center gap-md flex-wrap pt-sm empty:hidden">
+				{#each tags as tag}
+					<div class="bg-grey-200 py-1 px-2 w-fit rounded">
+						<span class="text-grey-700 text-sm font-medium">{tag}</span>
+					</div>
+				{/each}
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+</a>
---
+++ src/routes/app/projects/[slug]/+page.server.ts
@@ -0,0 +1,29 @@
+import { getSupabase } from "@supabase/auth-helpers-sveltekit";
+import { error, redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load = (async (event) => {
+  const { session, supabaseClient } = await getSupabase(event)
+  if (!session) {
+    throw redirect(303, "/")
+  }
+
+  let params = event.params
+
+  const projectId = params.slug
+
+  // Grabs project info 
+  const { data: project, error: errProject } = await supabaseClient.from('projects').select().eq('user_id', session.user.id).eq("id", projectId).limit(1).single()
+  // if (project?.user_id !== session.user.id) throw redirect(301, "/app/projects/not-invited")
+  if (project?.user_id !== session.user.id) throw error(401, "User is not owner or not invited")
+  if (project) {
+    return {
+      name: project?.project_name,
+      description: project?.description,
+      banner: project?.banner,
+      tags: JSON.parse(project?.tags) || []
+    }
+  }
+
+  throw error(404, errProject?.message)
+}) satisfies PageServerLoad
