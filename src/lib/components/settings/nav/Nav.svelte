217ebc0db31b5423d5e6201439df788deda3a811
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-09 20:12:36 -0700

    Made settings nav match mockup on desktop

--- src/lib/components/settings/nav/Nav.svelte
+++ src/lib/components/settings/nav/Nav.svelte
@@ -7,21 +7,33 @@
 	import User from '$lib/assets/Fill/User.svelte';
 </script>
 
-<nav class="relative top-14 flex flex-col gap-4 z-50">
-	<a href="#" class="gap-md py-sm px-md font-bold text-grey-700 flex items-center">
-		<User className="w-8 h-8 fill-grey-800" />
+<nav class="relative top-14 flex flex-col gap-4 z-50 md:flex-row md:top-2">
+	<a
+		href="#"
+		class="gap-md py-sm px-md font-bold text-grey-700 flex items-center active:bg-grey-200 rounded-md"
+	>
+		<User className="w-8 h-8 fill-grey-800 md:hidden" />
 		Account
 	</a>
-	<a href="#" class="gap-md py-sm px-md font-bold text-grey-700 flex items-center">
-		<Image className="w-8 h-8 fill-grey-800" />
+	<a
+		href="#"
+		class="gap-md py-sm px-md font-bold text-grey-700 flex items-cente active:bg-grey-200 rounded-mdr"
+	>
+		<Image className="w-8 h-8 fill-grey-800 md:hidden" />
 		Appearance
 	</a>
-	<a href="#" class="gap-md py-sm px-md font-bold text-grey-700 flex items-center">
-		<Bell className="w-8 h-8 fill-grey-800" />
+	<a
+		href="#"
+		class="gap-md py-sm px-md font-bold text-grey-700 flex items-center active:bg-grey-200 rounded-md"
+	>
+		<Bell className="w-8 h-8 fill-grey-800 md:hidden" />
 		Notifications
 	</a>
-	<a href="#" class="gap-md py-sm px-md font-bold text-grey-700 flex items-center">
-		<CircleInfo className="w-8 h-8 fill-grey-800" />
+	<a
+		href="#"
+		class="gap-md py-sm px-md font-bold text-grey-700 flex items-center active:bg-grey-200 rounded-md"
+	>
+		<CircleInfo className="w-8 h-8 fill-grey-800 md:hidden" />
 		About
 	</a>
 </nav>
