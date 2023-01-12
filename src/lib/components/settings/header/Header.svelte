533c568bb366071c6fb5f85d081fdefe7bee0ba8
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-12 07:34:46 -0700

    Added show menu button to settings mobile

--- src/lib/components/settings/header/Header.svelte
+++ src/lib/components/settings/header/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Menu from '$lib/assets/Menu.svelte';
 	import User from '$lib/assets/User.svelte';
 	export let data: any;
 </script>
@@ -7,7 +8,7 @@
 	style="background-image: url({data.banner});"
 	class="object-cover relative rounded-b-3xl md:px-8 px-6 h-[15.625rem] flex items-end bg-center"
 >
-	<div class="flex items-start gap-md absolute -bottom-12 w-auto md:-bottom-8 pr-6">
+	<div class="flex items-start gap-md absolute -bottom-12 w-[calc(100%-48px)] md:-bottom-8">
 		{#if data.avatar_url}
 			<img
 				src={data.avatar_url}
@@ -18,11 +19,19 @@
 			<User className="w-[6.25rem] h-[6.25rem] stroke-grey-700" />
 		{/if}
 
-		<div class="mt-sm md:mt-md md:w-auto flex flex-col z-50 h-full w-full overflow-hidden">
+		<div class="mt-sm md:mt-md md:w-auto flex flex-col z-50 w-full h-full overflow-hidden">
 			<h1 class="text-grey-100 text-lg md:text-xl">Settings</h1>
-			<span class="md:hidden text-grey-700 font-bold inline-block">{data.name}</span>
-			<span class="md:hidden text-grey-700 font-medium inline-block truncate">{data.email}</span>
-
+			<div class="md:hidden flex w-full gap-md">
+				<div class="flex flex-col w-3/4">
+					<span class="md:hidden text-grey-700 font-bold inline-block truncate">{data.name}</span>
+					<span class="md:hidden text-grey-700 font-medium inline-block truncate">{data.email}</span
+					>
+				</div>
+				<button class="ml-auto">
+					<Menu className="w-8 h-8 stroke-grey-700" />
+					<span class="sr-only">Show nav menu</span>
+				</button>
+			</div>
 			<span class="text-grey-700 font-bold hidden md:block truncate"
 				>{data.name} <span class="font-medium">- {data.email}</span>
 			</span>
