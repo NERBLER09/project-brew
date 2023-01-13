a720b1128f1c818fbe6a7fea077d19022e8d32be
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-12 18:38:01 -0700

    Added in external links to about page

--- src/routes/app/settings/about/+page.svelte
+++ src/routes/app/settings/about/+page.svelte
@@ -7,7 +7,7 @@
 		</div>
 	</header>
 	<div class="flex flex-col gap-sm">
-		<a href="#">Submit an issue</a>
+		<a href="https://github.com/NERBLER09/project-brew/issues/new" target="_blank" rel="noreferrer" class="text-grey-700 font-medium">Submit an issue</a>
 		<a href="#">Privacy Policy</a>
 	</div>
 </section>
@@ -16,6 +16,6 @@
 		<h2 class="text-lg text-grey-900 font-semibold">Credits</h2>
 	</header>
 	<div>
-		<p class="font-medium text-grey-700">Icons sourced from Ripple Icons from Flowbase</p>
+		<p class="font-medium text-grey-700">Icons sourced from <a href="https://github.com/NERBLER09/project-brew/issues/new" target="_blank" rel="noreferrer" class="underline">Ripple Icons</a> from Flowbase</p>
 	</div>
 </section>
