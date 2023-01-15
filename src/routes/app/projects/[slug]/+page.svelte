763ebc1afcd45ee596466221fac3271efee362c6
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-14 17:22:45 -0700

    Fixed opened project header not properly handling scrolling

--- src/routes/app/projects/[slug]/+page.svelte
+++ src/routes/app/projects/[slug]/+page.svelte
@@ -11,58 +11,59 @@
 	currentProject.set(data);
 </script>
 
-<header class="h-[13.5rem]">
-	<div class="relative z-50">
-		<div class="flex items-center mb-md md:mb-sm md:items-start">
-			<a class="flex items-center gap-md" href="/app/projects">
-				<Back
-					className="w-8 h-8 aspect-square {data.banner
-						? 'stroke-grey-200'
-						: 'stroke-grey-700'} md:h-10 md:w-10"
-				/>
-				<h1
-					class="w-fit text-lg {data.banner
-						? 'text-grey-200'
-						: 'text-grey-700'} truncate md:text-xl md:font-semibold"
-				>
-					{data.name}
-				</h1>
-			</a>
-			<div class="flex items-center gap-md ml-auto">
-				<a href="/app/projects/{data.id}/about">
-					<CircleInfo className="w-8 h-8 {data.banner ? 'stroke-grey-200' : 'stroke-grey-700'}" />
-					<span class="sr-only">View project info</span>
-				</a>
-				<MoreHorizontal className="w-8 h-8 {data.banner ? 'stroke-grey-200' : 'stroke-grey-700'}" />
-			</div>
-		</div>
-		<div class="flex flex-wrap gap-md mb-md">
-			{#each data.tags as tag}
-				<div class="bg-grey-200 py-1 px-2 w-fit rounded">
-					<span class="text-grey-700 text-sm font-medium">{tag}</span>
-				</div>
-			{/each}
-		</div>
-		<div>
-			<p
-				class="font-medium max-h-[6ch] text-ellipsis overflow-hidden {data.banner
+<header
+	class="min-h-[13.5rem] object-cover rounded-b-3xl relative -top-6 -left-6 p-4 w-[calc(100%+48px)] md:-top-8 md:-left-8 md:w-[calc(100%+64px)] md:p-8"
+	style="background-image: url({data.banner});"
+>
+	<div class="flex items-center mb-md md:mb-sm md:items-start">
+		<a class="flex items-center gap-md" href="/app/projects">
+			<Back
+				className="w-8 h-8 aspect-square {data.banner
+					? 'stroke-grey-200'
+					: 'stroke-grey-700'} md:h-10 md:w-10"
+			/>
+			<h1
+				class="w-fit text-lg {data.banner
 					? 'text-grey-200'
-					: 'text-grey-700'} text-sm md:text-base"
+					: 'text-grey-700'} truncate md:text-xl md:font-semibold"
 			>
-				{data.description}
-			</p>
-			<span class="font-medium {data.banner ? 'text-grey-200' : 'text-grey-700'} text-sm mt-sm"
-				>Read more</span
-			>
+				{data.name}
+			</h1>
+		</a>
+		<div class="flex items-center gap-md ml-auto">
+			<a href="/app/projects/{data.id}/about">
+				<CircleInfo className="w-8 h-8 {data.banner ? 'stroke-grey-200' : 'stroke-grey-700'}" />
+				<span class="sr-only">View project info</span>
+			</a>
+			<MoreHorizontal className="w-8 h-8 {data.banner ? 'stroke-grey-200' : 'stroke-grey-700'}" />
 		</div>
 	</div>
-	<img
-		src={data.banner}
-		alt="Project banner"
-		class="absolute top-0 left-0 h-[15.625rem] w-full md:w-[calc(100%-16.625rem)] md:left-auto md:right-0 object-cover rounded-b-3xl {data.banner
-			? 'block'
-			: 'hidden'}"
-	/>
+	<div class="flex flex-wrap gap-md mb-md">
+		{#each data.tags as tag}
+			<div class="bg-grey-200 py-1 px-2 w-fit rounded">
+				<span class="text-grey-700 text-sm font-medium">{tag}</span>
+			</div>
+		{/each}
+	</div>
+	<div>
+		<p
+			class="font-medium max-h-[6ch] text-ellipsis overflow-hidden {data.banner
+				? 'text-grey-200'
+				: 'text-grey-700'} text-sm md:text-base"
+		>
+			{data.description}
+		</p>
+		<span class="font-medium {data.banner ? 'text-grey-200' : 'text-grey-700'} text-sm mt-sm"
+			>Read more</span
+		>
+	</div>
+	<!-- <img -->
+	<!-- 	src={data.banner} -->
+	<!-- 	alt="Project banner" -->
+	<!-- 	class="absolute top-0 left-0 h-[15.625rem] w-full md:w-[calc(100%-16.625rem)] md:left-auto md:right-0 object-cover rounded-b-3xl {data.banner -->
+	<!-- 		? 'block' -->
+	<!-- 		: 'hidden'}" -->
+	<!-- /> -->
 </header>
 
 <section
