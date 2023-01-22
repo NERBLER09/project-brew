db33e5db62d1bb26785ba333cdd6a20985e239e0
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-22 09:42:56 -0700

    Added view info function

--- src/lib/components/dropdowns/projects/ProjectDropdown.svelte
+++ src/lib/components/dropdowns/projects/ProjectDropdown.svelte
@@ -3,6 +3,7 @@
 	import Filter from '$lib/assets/Filter.svelte';
 	import Trash from '$lib/assets/Trash.svelte';
 	import User from '$lib/assets/User.svelte';
+	import { currentProject } from '$lib/stores/project';
 
 	export let visibility = false;
 </script>
@@ -12,10 +13,10 @@
 		<Filter className="dropdown--icon" parentBg="grey-100" />
 		<span class="dropdown--label">Filter</span>
 	</button>
-	<button class="dropdown--item" on:click={() => (visibility = false)}>
+	<a class="dropdown--item" href="/app/projects/{$currentProject.id}/about">
 		<CircleInfo className="dropdown--icon" />
 		<span class="dropdown--label">View Info</span>
-	</button>
+	</a>
 	<button class="dropdown--item" on:click={() => (visibility = false)}>
 		<User className="dropdown--icon" />
 		<span class="dropdown--label">View invited members</span>
