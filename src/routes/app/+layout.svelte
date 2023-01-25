d204c84f7a58448bb9ab2f6b755ac91cdef091de
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-25 07:11:35 -0700

    Setup dark mode in tailwind config

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -5,9 +5,18 @@
 	import MobileNavbar from '$lib/components/Sidebar/MobileNavbar.svelte';
 
 	import { showMobileNav } from '$lib/stores/ui';
+	import { onMount } from 'svelte';
+
+	let useDarkMode = false;
+
+	onMount(() => {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			useDarkMode = true;
+		}
+	});
 </script>
 
-<div class="flex h-screen flex-col-reverse md:flex-row">
+<div class="flex h-screen flex-col-reverse md:flex-row" class:dark={useDarkMode}>
 	<aside>
 		<!-- Each automaticly hide based on screen size in tailwind -->
 		<DesktopSidebar />
