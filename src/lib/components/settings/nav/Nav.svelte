b44813ad7a19714d87ef35d57840d1d3957a9e10
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-26 07:09:04 -0700

    Added dark mode support to settings nav + header

--- src/lib/components/settings/nav/Nav.svelte
+++ src/lib/components/settings/nav/Nav.svelte
@@ -8,30 +8,30 @@
 <nav class="relative top-14 flex flex-col gap-4 z-50 md:flex-row md:top-2">
 	<a
 		href="/app/settings/account"
-		class="gap-md py-sm px-md font-bold text-grey-700 flex items-center active:bg-grey-200 rounded-md"
+		class="gap-md py-sm px-md font-bold text-grey-700 dark:text-grey-200 flex items-center active:bg-grey-200 dark:active:bg-grey-700 rounded-md"
 	>
-		<User className="w-8 h-8 fill-grey-800 md:hidden" />
+		<User className="w-8 h-8 fill-grey-800 dark:fill-grey-200 md:hidden" />
 		Account
 	</a>
 	<a
 		href="/app/settings/appearance"
-		class="gap-md py-sm px-md font-bold text-grey-700 flex items-center active:bg-grey-200 rounded-md"
+		class="gap-md py-sm px-md font-bold text-grey-700 dark:text-grey-200 flex items-center active:bg-grey-200 dark:active:bg-grey-700 rounded-md"
 	>
-		<Image className="w-8 h-8 fill-grey-800 md:hidden" />
+		<Image className="w-8 h-8 fill-grey-800 dark:fill-grey-200 md:hidden" />
 		Appearance
 	</a>
 	<a
 		href="/app/settings/notifications"
-		class="gap-md py-sm px-md font-bold text-grey-700 flex items-center active:bg-grey-200 rounded-md"
+		class="gap-md py-sm px-md font-bold text-grey-700 dark:text-grey-200 flex items-center active:bg-grey-200 dark:active:bg-grey-700 rounded-md"
 	>
-		<Bell className="w-8 h-8 fill-grey-800 md:hidden" />
+		<Bell className="w-8 h-8 fill-grey-800 dark:fill-grey-200 md:hidden" />
 		Notifications
 	</a>
 	<a
 		href="/app/settings/about"
-		class="gap-md py-sm px-md font-bold text-grey-700 flex items-center active:bg-grey-200 rounded-md"
+		class="gap-md py-sm px-md font-bold text-grey-700 dark:text-grey-200 flex items-center active:bg-grey-200 dark:active:bg-grey-700 rounded-md"
 	>
-		<CircleInfo className="w-8 h-8 fill-grey-800 md:hidden" />
+		<CircleInfo className="w-8 h-8 fill-grey-800 dark:fill-grey-200 md:hidden" />
 		About
 	</a>
 </nav>
