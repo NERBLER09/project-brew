7825579a066788b21dd123511e4d8f8eb38e2ae3
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-26 16:55:49 -0700

    Updated setting pages to support dark mode

--- src/routes/app/settings/appearance/+page.svelte
+++ src/routes/app/settings/appearance/+page.svelte
@@ -1,46 +1,71 @@
 <script lang="ts">
-	import Check from "$lib/assets/Check.svelte";
-	import CircleCheck from "$lib/assets/Fill/CircleCheck.svelte";
-    import Dark from "$lib/assets/Theme/Dark.svelte";
-    import Light from "$lib/assets/Theme/Light.svelte";
-    import System from "$lib/assets/Theme/System.svelte";
+	import Check from '$lib/assets/Check.svelte';
+	import CircleCheck from '$lib/assets/Fill/CircleCheck.svelte';
+	import Dark from '$lib/assets/Theme/Dark.svelte';
+	import Light from '$lib/assets/Theme/Light.svelte';
+	import System from '$lib/assets/Theme/System.svelte';
 
-    type Theme = "light" | "dark" | "system"
-    let selectedTheme: Theme = "light"
+	type Theme = 'light' | 'dark' | 'system';
+	let selectedTheme: Theme = 'light';
 </script>
+
 <section>
 	<header>
-		<h2 class="text-lg text-grey-700 font-semibold">Theme</h2>
+		<h2 class="text-lg text-grey-700 dark:text-grey-200 font-semibold">Theme</h2>
 	</header>
 
 	<div class="flex flex-wrap gap-6 mt-md">
-		<button class="relative text-start" on:click={() => selectedTheme = "light"}>
-            {#if selectedTheme === "light"}
-                <CircleCheck className="fill-accent-light w-8 h-8 absolute -top-4 -right-4"/>
-            {/if}
-            <div class="p-sm {selectedTheme === "light" ? 'border-accent-light' : 'border-grey-700'} border-2 rounded-lg mb-md">
-                <Light/>
-            </div>
-            <span class="{selectedTheme === "light" ? 'text-accent-light' : 'text-grey-700'} font-bold">Light</span>
-        </button>
-		<button class="relative text-start" on:click={() => selectedTheme = "dark"}>
-            {#if selectedTheme === "dark"}
-                <CircleCheck className="fill-accent-light w-8 h-8 absolute -top-4 -right-4"/>
-            {/if}
-            <div class="p-sm {selectedTheme === "dark" ? 'border-accent-light' : 'border-grey-700'} border-2 rounded-lg mb-md">
-                <Dark/>
-            </div>
-            <span class="{selectedTheme === "dark" ? 'text-accent-light' : 'text-grey-700'} font-bold">Dark</span>
-        </button>
-		<button class="relative text-start" on:click={() => selectedTheme = "system"}>
-            {#if selectedTheme === "system"}
-                <CircleCheck className="fill-accent-light w-8 h-8 absolute -top-4 -right-4"/>
-            {/if}
-            <div class="p-sm {selectedTheme === "system" ? 'border-accent-light' : 'border-grey-700'} border-2 rounded-lg mb-md">
-                <System/>
-            </div>
-            <span class="{selectedTheme === "system" ? 'text-accent-light' : 'text-grey-700'} font-bold">System</span>
-        </button>
+		<button class="relative text-start" on:click={() => (selectedTheme = 'light')}>
+			{#if selectedTheme === 'light'}
+				<CircleCheck className="fill-accent-light w-8 h-8 absolute -top-4 -right-4" />
+			{/if}
+			<div
+				class="p-sm {selectedTheme === 'light'
+					? 'border-accent-light'
+					: 'border-grey-700 dark:border-grey-200'} border-2 rounded-lg mb-md"
+			>
+				<Light />
+			</div>
+			<span
+				class="{selectedTheme === 'light'
+					? 'text-accent-light'
+					: 'text-grey-700 dark:text-grey-200'} font-bold">Light</span
+			>
+		</button>
+		<button class="relative text-start" on:click={() => (selectedTheme = 'dark')}>
+			{#if selectedTheme === 'dark'}
+				<CircleCheck className="fill-accent-light w-8 h-8 absolute -top-4 -right-4" />
+			{/if}
+			<div
+				class="p-sm {selectedTheme === 'dark'
+					? 'border-accent-light'
+					: 'border-grey-700 dark:border-grey-200'} border-2 rounded-lg mb-md"
+			>
+				<Dark />
+			</div>
+			<span
+				class="{selectedTheme === 'dark'
+					? 'text-accent-light'
+					: 'text-grey-700 dark:text-grey-200'} font-bold">Dark</span
+			>
+		</button>
+		<button class="relative text-start" on:click={() => (selectedTheme = 'system')}>
+			{#if selectedTheme === 'system'}
+				<CircleCheck className="fill-accent-light w-8 h-8 absolute -top-4 -right-4" />
+			{/if}
+			<div
+				class="p-sm {selectedTheme === 'system'
+					? 'border-accent-light'
+					: 'border-grey-700 dark:border-grey-200'} border-2 rounded-lg mb-md"
+			>
+				<System />
+			</div>
+			<span
+				class="{selectedTheme === 'system'
+					? 'text-accent-light'
+					: 'text-grey-700 dark:text-grey-200'} font-bold">System</span
+			>
+		</button>
 
 		<button class="md:hidden button--circle bottom-32 right-8 fixed z-50">
 			<Check className="h-8 w-8 stroke-grey-200" />
