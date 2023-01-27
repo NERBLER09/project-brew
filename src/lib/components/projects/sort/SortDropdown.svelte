6eafd853a30c6071a4eddeb90815847644b483ac
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-01-26 17:25:25 -0700

    Dark mode support for dropdowns

--- src/lib/components/projects/sort/SortDropdown.svelte
+++ src/lib/components/projects/sort/SortDropdown.svelte
@@ -15,21 +15,20 @@
 </script>
 
 <div
-	class="bg-grey-100 rounded-lg flex flex-col p-md gap-4 min-w-max border-2 border-grey-700 w-[12.5rem] md:w-[10.3125rem] md:border-t-0"
+	class="bg-grey-100 dark:bg-grey-800 rounded-lg flex flex-col p-md gap-4 min-w-max border-2 border-grey-700 w-[12.5rem] md:w-[10.3125rem] md:border-t-0"
 >
 	<header class="md:hidden">
-		<h3 class="text-grey-700 font-semibold text-md">Sort Options</h3>
+		<h3 class="text-grey-700 dark:text-grey-200 font-semibold text-md">Sort Options</h3>
 	</header>
 
 	<div class="flex flex-col gap-md items-start justify-start">
-		<p class="text-grey-700 font-medium md:hidden">Sorting by: {$projectSort}</p>
-		<button
-			class="font-bold text-grey-700 p-sm m-sm hover:bg-grey-200 active:bg-grey-200 w-full text-start rounded-sm"
-			on:click={handleAZSort}>A-Z</button
-		>
-		<button
-			class="font-bold text-grey-700 p-sm m-sm hover:bg-grey-200 active:bg-grey-200 w-full text-start rounded-sm"
-			on:click={handleZASort}>Z-A</button
-		>
+		<p class="text-grey-700 dark:text-grey-200 font-medium md:hidden">Sorting by: {$projectSort}</p>
+		<button class="dropdown--item w-full" on:click={handleAZSort}>
+			<span class="dropdown--label">A-Z</span>
+		</button>
+
+		<button class="dropdown--item w-full" on:click={handleZASort}>
+			<span class="dropdown--label">Z-A</span>
+		</button>
 	</div>
 </div>
--- src/lib/components/text/Description.svelte
+++ src/lib/components/text/Description.svelte
@@ -1,40 +1,46 @@
 <script lang="ts">
-    export let description = ""
-    export let banner = ""
+	export let description = '';
+	export let banner = '';
 
-    let expandContents = false
-    let contentOverflowing = false
+	let expandContents = false;
+	let contentOverflowing = false;
 
-    let descriptionElement: HTMLElement
-    const checkForOverflow = (element: HTMLElement) => {
-        if(!element) return
-        contentOverflowing = element.offsetHeight < element.scrollHeight || element.scrollWidth > element.clientWidth 
-        console.log(contentOverflowing)
-    }
+	let descriptionElement: HTMLElement;
+	const checkForOverflow = (element: HTMLElement) => {
+		if (!element) return;
+		contentOverflowing =
+			element.offsetHeight < element.scrollHeight || element.scrollWidth > element.clientWidth;
+		console.log(contentOverflowing);
+	};
 
-    $: checkForOverflow(descriptionElement)
+	$: checkForOverflow(descriptionElement);
 </script>
-<svelte:window on:resize={() => checkForOverflow(descriptionElement)}></svelte:window>
-<div>
-    <p
-        class="font-medium min-h-fit {expandContents ? 'h-fit' : 'max-h-[6ch]'} text-ellipsis overflow-hidden {banner
-            ? 'text-grey-200'
-            : 'text-grey-700'} text-sm sm:text-base"
-        bind:this={descriptionElement}
-    >
-        {description}
-    </p>
-    {#if contentOverflowing}
-        <button
-            class="font-medium {banner ? 'text-grey-200' : 'text-grey-700'} text-sm mt-sm"
-            on:click={() => expandContents = !expandContents}
-        >
-            {#if expandContents}
-                <span>Hide</span> 
-            {:else}
-                <span>Show more</span> 
-            {/if}
-        </button>       
-    {/if}
 
+<svelte:window on:resize={() => checkForOverflow(descriptionElement)} />
+<div>
+	<p
+		class="font-medium min-h-fit {expandContents
+			? 'h-fit'
+			: 'max-h-[6ch]'} text-ellipsis overflow-hidden {banner
+			? 'text-grey-200'
+			: 'text-grey-700 dark:text-grey-200'} text-sm sm:text-base"
+		bind:this={descriptionElement}
+	>
+		{description}
+	</p>
+	{#if contentOverflowing}
+		<button
+			class="font-medium {banner
+				? 'text-grey-200'
+				: 'text-grey-700 dark:text-grey-200'} text-sm mt-sm"
+			on:click={() => (expandContents = !expandContents)}
+		>
+			{#if expandContents}
+				<span>Hide</span>
+			{:else}
+				<span>Show more</span>
+			{/if}
+		</button>
+	{/if}
 </div>
+
