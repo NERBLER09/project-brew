54fdf9cfc19c6bcfb09f6b7d965e5652b5b1932b
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-04 20:59:32 -0700

    Added very basic bar graph-like data columns

--- src/lib/components/home/activity/YourActivity.svelte
+++ src/lib/components/home/activity/YourActivity.svelte
@@ -12,39 +12,48 @@
 		</p>
 	</div>
 
-	<div class="h-[calc(100%-5.5625rem)]">
+	<div class="relative h-[calc(100%-5.5625rem)]">
 		<div class="flex h-full flex-col-reverse justify-around text-end">
 			<div class="flex items-center gap-md">
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">0</span>
-				<div class="h-1 w-full rounded-full bg-grey-400" />
+				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
 			<div class="flex items-center gap-md">
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">5</span>
-				<div class="h-1 w-full rounded-full bg-grey-400" />
+				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
 			<div class="flex items-center gap-md">
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">10</span>
-				<div class="h-1 w-full rounded-full bg-grey-400" />
+				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
 			<div class="flex items-center gap-md">
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">15</span>
-				<div class="h-1 w-full rounded-full bg-grey-400" />
+				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
 			<div class="flex items-center gap-md">
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">20</span>
-				<div class="h-1 w-full rounded-full bg-grey-400" />
+				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
 			<div class="flex items-center gap-md">
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">25</span>
-				<div class="h-1 w-full rounded-full bg-grey-400" />
+				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
 			<div class="flex items-center gap-md">
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">30</span>
-				<div class="h-1 w-full rounded-full bg-grey-400" />
+				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
 		</div>
 
-		<div class="flex items-center justify-around">
+		<div class="absolute bottom-2 left-[1.75rem] flex h-full w-[calc(100%-1.75rem)] justify-around">
+			<div class="h-full w-1 rounded-full bg-accent-light" />
+			<div class="h-full w-1 rounded-full bg-accent-light" />
+			<div class="h-full w-1 rounded-full bg-accent-light" />
+			<div class="h-full w-1 rounded-full bg-accent-light" />
+			<div class="h-full w-1 rounded-full bg-accent-light" />
+			<div class="h-full w-1 rounded-full bg-accent-light" />
+		</div>
+
+		<div class="ml-auto flex w-[calc(100%-1.75rem)] items-center justify-between">
 			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 01</span></div>
 			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span></div>
 			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 03</span></div>
