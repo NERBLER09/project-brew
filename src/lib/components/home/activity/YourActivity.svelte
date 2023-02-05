ef57485a3a66128538896847a606489b42642da0
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-05 09:28:02 -0700

    Display mock data in your activity section

--- src/lib/components/home/activity/YourActivity.svelte
+++ src/lib/components/home/activity/YourActivity.svelte
@@ -1,5 +1,56 @@
-<script>
+<script lang="ts">
 	import { tasksCompletedThisDay } from '$lib/stores/project';
+	import { onMount } from 'svelte';
+
+	let currentDate: string;
+
+	const mockData = [
+		{
+			date: '1-1-2023',
+			formattedDate: 'Jan 01',
+			tasksCompleted: 8
+		},
+		{
+			date: '2-1-2023',
+			formattedDate: 'Jan 02',
+			tasksCompleted: 17
+		},
+		{
+			date: '3-1-2023',
+			formattedDate: 'Jan 03',
+			tasksCompleted: 10
+		},
+		{
+			date: '4-1-2023',
+			formattedDate: 'Jan 04',
+			tasksCompleted: 20
+		},
+		{
+			date: '5-1-2023',
+			formattedDate: 'Jan 05',
+			tasksCompleted: 10
+		},
+		{
+			date: '6-1-2023',
+			formattedDate: 'Jan 06',
+			tasksCompleted: 25
+		},
+		{
+			date: '5-2-2023',
+			formattedDate: 'Feb 05',
+			tasksCompleted: 5
+		}
+	];
+
+	onMount(() => {
+		const date = new Date();
+
+		let day = date.getDate();
+		let month = date.getMonth() + 1;
+		let year = date.getFullYear();
+
+		currentDate = `${day}-${month}-${year}`;
+	});
 </script>
 
 <section class="col-span-2 row-span-1 hidden md:block">
@@ -38,45 +89,23 @@
 				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">25</span>
 				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
 			</div>
-			<div class="flex items-center gap-md">
-				<span class="w-[2ch] text-sm font-bold text-grey-600 dark:text-grey-400">30</span>
-				<div class="h-1 w-full rounded-full bg-grey-400 dark:bg-grey-700" />
-			</div>
 		</div>
 
 		<div class="ml-auto flex w-[calc(100%-1.75rem)] items-center justify-between">
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 01</span>
-			</div>
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span>
-			</div>
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span>
-			</div>
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span>
-			</div>
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span>
-			</div>
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span>
-			</div>
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span>
-			</div>
-			<div class="flex flex-col items-center">
-				<div class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2" />
-				<span class="text-sm font-bold text-grey-700 dark:text-grey-200">Today</span>
-			</div>
+			{#each mockData as { date, formattedDate, tasksCompleted }}
+				<div class="flex flex-col items-center">
+					<div
+						class="max-h-[calc(100%-1rem)] min-h-[0.5rem] w-1 rounded-full bg-accent-light absolute bottom-2"
+						style="height: {(tasksCompleted / 25) * 100}%"
+					/>
+					{#if date === currentDate}
+						<span class="text-sm font-bold text-grey-700 dark:text-grey-200">Today</span>
+					{:else}
+						<span class="text-sm font-medium text-grey-700 dark:text-grey-200">{formattedDate}</span
+						>
+					{/if}
+				</div>
+			{/each}
 		</div>
 	</div>
 </section>
