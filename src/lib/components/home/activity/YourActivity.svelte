37ce7b56456396ad5635275629402986acb8d885
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-04 18:03:58 -0700

    Created simple empty graph outline

--- src/lib/components/home/activity/YourActivity.svelte
+++ src/lib/components/home/activity/YourActivity.svelte
@@ -4,11 +4,54 @@
 
 <section class="col-span-2 row-span-1 hidden md:block">
 	<header>
-		<h2 class="text-md font-semibold text-grey-800 dark:text-grey-100 md:text-lg">Your Activity</h2>
+		<h2 class="font-semibold text-grey-800 dark:text-grey-100 md:text-lg">Your Activity</h2>
 	</header>
 	<div>
-		<p class="font-medium text-grey-700 dark:text-grey-200">
+		<p class="my-md font-medium text-grey-700 dark:text-grey-200">
 			You've completed {$tasksCompletedThisDay} tasks today.
 		</p>
+	</div>
+
+	<div class="h-[calc(100%-5.5625rem)]">
+		<div class="flex h-full flex-col-reverse justify-around text-end">
+			<div class="flex items-center gap-md">
+				<span class="w-[2ch] text-sm font-bold text-grey-600">0</span>
+				<div class="h-1 w-full rounded-full bg-grey-400" />
+			</div>
+			<div class="flex items-center gap-md">
+				<span class="w-[2ch] text-sm font-bold text-grey-600">5</span>
+				<div class="h-1 w-full rounded-full bg-grey-400" />
+			</div>
+			<div class="flex items-center gap-md">
+				<span class="w-[2ch] text-sm font-bold text-grey-600">10</span>
+				<div class="h-1 w-full rounded-full bg-grey-400" />
+			</div>
+			<div class="flex items-center gap-md">
+				<span class="w-[2ch] text-sm font-bold text-grey-600">15</span>
+				<div class="h-1 w-full rounded-full bg-grey-400" />
+			</div>
+			<div class="flex items-center gap-md">
+				<span class="w-[2ch] text-sm font-bold text-grey-600">20</span>
+				<div class="h-1 w-full rounded-full bg-grey-400" />
+			</div>
+			<div class="flex items-center gap-md">
+				<span class="w-[2ch] text-sm font-bold text-grey-600">25</span>
+				<div class="h-1 w-full rounded-full bg-grey-400" />
+			</div>
+			<div class="flex items-center gap-md">
+				<span class="w-[2ch] text-sm font-bold text-grey-600">30</span>
+				<div class="h-1 w-full rounded-full bg-grey-400" />
+			</div>
+		</div>
+
+		<div class="flex items-center justify-around">
+			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 01</span></div>
+			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 02</span></div>
+			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 03</span></div>
+			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 04</span></div>
+			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 05</span></div>
+			<div><span class="text-sm font-medium text-grey-700 dark:text-grey-200">Jan 06</span></div>
+			<div><span class="text-sm font-bold text-grey-700 dark:text-grey-200">Today</span></div>
+		</div>
 	</div>
 </section>
