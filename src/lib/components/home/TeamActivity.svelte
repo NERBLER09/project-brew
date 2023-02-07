f2ce8a6b4a71afb7d78c50043f0db26bfb1c0413
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-07 07:23:46 -0700

    Fixed minor ui issue

--- src/lib/components/home/TeamActivity.svelte
+++ src/lib/components/home/TeamActivity.svelte
@@ -1,4 +1,4 @@
-<section class="col-span-1 row-start-3 md:col-start-2 md:row-start-4 lg:col-start-5 lg:row-start-1">
+<section class="col-span-1 row-start-3 md:col-start-2 md:row-start-5 lg:col-start-5 lg:row-start-1">
 	<header>
 		<h2 class="text-md font-semibold text-grey-800 dark:text-grey-100 md:text-lg">Team Activity</h2>
 	</header>
