7644e6568de4f98959c218ff5bc37421a09cdd47
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-16 14:02:07 -0700

    Added header to dropdown

--- src/lib/components/notifications/DesktopNotifications.svelte
+++ src/lib/components/notifications/DesktopNotifications.svelte
@@ -1,6 +1,9 @@
 <div
 	class="absolute right-0 top-8 z-40 h-fit min-h-[12.5rem] w-[18.75rem] rounded-lg bg-grey-100 p-3 dark:bg-grey-800"
 >
+	<header class="mb-sm">
+		<h3 class="text-md font-bold text-grey-800 dark:text-grey-200">Notifications</h3>
+	</header>
 	<div class="flex flex-col items-center gap-md">
 		<span class="text-md font-bold text-grey-700 dark:text-grey-200">You are all caught up</span>
 		<span class="font-medium text-grey-700 dark:text-grey-200">You have no new notifications</span>
