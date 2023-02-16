2a424425c6a9c4fd4bfd8bb9475c0cadef10d9ab
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-15 20:43:50 -0700

    Setup realtime for project

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,11 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 
-let supabase = createClient(import.meta.env.VITE_PUBLIC_SUPABSE_URL, import.meta.env.VITE_PUBLIC_SUPABSE_ANON_KEY);
+let supabase = createClient(import.meta.env.VITE_PUBLIC_SUPABSE_URL, import.meta.env.VITE_PUBLIC_SUPABSE_ANON_KEY, {
+  realtime: {
+    params: {
+      eventsPerSecond: 10
+    }
+  }
+});
 
 export { supabase };
