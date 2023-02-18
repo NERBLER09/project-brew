6e3e131dc69ceddaf6950d79f6d8a35bbd3bcac0
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-18 13:42:34 -0700

    Reworked login in handle to use sveltekit form actions

---
+++ src/routes/(landing-page)/login/+page.server.ts
@@ -0,0 +1,20 @@
+import { getSupabase } from "@supabase/auth-helpers-sveltekit";
+import { fail, redirect, type Actions } from "@sveltejs/kit";
+
+export const actions = {
+  default: async (event) => {
+    const { supabaseClient } = await getSupabase(event)
+
+    const data = await event.request.formData()
+    const email = data.get("email") as string
+    const password = data.get("password") as string
+
+    const { error: err } = await supabaseClient.auth.signInWithPassword({ email, password })
+    if (err) {
+      throw fail(403, { message: err.message, fail: true })
+    }
+    else {
+      throw redirect(303, "/app/home")
+    }
+  }
+} satisfies Actions
--- src/routes/(landing-page)/login/+page.svelte
+++ src/routes/(landing-page)/login/+page.svelte
@@ -1,20 +1,8 @@
 <script>
-	import { handleEmailPasswordLogin, handleGoogleAuth } from '$lib/api/auth';
+	import { handleGoogleAuth } from '$lib/api/auth';
+	import { enhance } from '$app/forms';
 
 	import Google from '$lib/assets/Logo/Google.svelte';
-
-	let email = '';
-	let password = '';
-
-	let errorMsg = '';
-
-	const handleAuth = async () => {
-		try {
-			await handleEmailPasswordLogin(email, password);
-		} catch (error) {
-			error = error;
-		}
-	};
 </script>
 
 <svelte:head>
@@ -22,20 +10,27 @@
 </svelte:head>
 
 <section
-	class="text-center mx-6 h-screen md:w-screen place-content-center grid overflow-hidden absolute top-0"
+	class="absolute top-0 mx-6 grid h-screen place-content-center overflow-hidden text-center md:w-screen"
 >
 	<header>
-		<h1 class="text-grey-800 font-semibold text-xl sm:text-2xl">Log in</h1>
+		<h1 class="text-xl font-semibold text-grey-800 sm:text-2xl">Log in</h1>
 	</header>
-	<form on:submit|preventDefault={handleAuth} class="flex gap-[0.625rem] flex-col items-center">
-		<input type="email" class="input--text w-full md:w-72" placeholder="Email" bind:value={email} />
+	<form method="POST" class="flex flex-col items-center gap-[0.625rem]" use:enhance>
+		<input
+			type="email"
+			class="input--text w-full md:w-72"
+			placeholder="Email"
+			name="email"
+			required
+		/>
+
 		<input
 			type="password"
 			class="input--text w-full md:w-72"
 			placeholder="Password"
-			bind:value={password}
+			name="password"
+			required
 		/>
-		<span class="text-sm font-medium empty:hidden">{errorMsg}</span>
 		<button class="button--primary" type="submit">Log in</button>
 		<span>or</span>
 		<button
@@ -43,7 +38,7 @@
 			type="button"
 			on:click={handleGoogleAuth}><Google className="h-lg w-lg" /> Continue with Google</button
 		>
-		<p class="text-sm text-grey-700 px-6 md:w-72">
+		<p class="px-6 text-sm text-grey-700 md:w-72">
 			By signing up with email+password you agree to our privacy policy
 		</p>
 	</form>
