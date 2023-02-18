712ff967d32bb4e8cefc6034539573ead070e503
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-18 13:48:08 -0700

    Display message when user has no invited team members

--- src/routes/app/team/+page.svelte
+++ src/routes/app/team/+page.svelte
@@ -34,7 +34,11 @@
 <div class="mt-md flex w-full flex-col items-start gap-lg md:grid md:grid-cols-2 lg:grid-cols-4">
 	{#each data.members as id}
 		<TeamMemberLink {id} />
+	{:else}
+		<p class="font-medium text-grey-700 dark:text-grey-200">
+			People you have added to your team will appear here. To add a person click on the "invite"
+			button.
+		</p>
 	{/each}
 </div>
-
 <InviteMember bind:shown={showInvitePrompt} />
