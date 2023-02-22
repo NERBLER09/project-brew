33f780a66dbec3cf0f72bd8342b8185f0a39330a
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-22 07:00:59 -0700

    Fixed issue where tags display improperly

--- src/lib/components/projects/tags/TagList.svelte
+++ src/lib/components/projects/tags/TagList.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import TagItem from './TagItem.svelte';
 
 	export let tags: string[];
+	onMount(() => {
+		try {
+			tags = JSON.parse(tags.toString());
+		} catch {}
+	});
 </script>
 
 <div class="mb-md flex flex-wrap gap-md">
