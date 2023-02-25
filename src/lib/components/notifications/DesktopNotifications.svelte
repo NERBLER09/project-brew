494e5e22f31d7a342b7c369d490790c96744a22e
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-25 15:04:54 -0700

    Fixed notifications overflowing on desktop

--- src/lib/components/notifications/DesktopNotifications.svelte
+++ src/lib/components/notifications/DesktopNotifications.svelte
@@ -41,23 +41,26 @@
 </script>
 
 <div
-	class="absolute right-0 top-8 z-40 h-fit max-h-[25rem] min-h-[12.5rem] w-[18.75rem] rounded-lg bg-grey-100 p-3 dark:bg-grey-800"
+	class="absolute right-0 top-8 z-40 h-fit max-h-[25rem] min-h-[12.5rem] w-[18.75rem] overflow-y-auto rounded-lg bg-grey-100 p-3 dark:bg-grey-800"
 >
-	<header class="mb-sm">
+	<header class="sticky -top-3 mb-sm bg-grey-100 dark:bg-grey-800">
 		<h3 class="text-md font-semibold text-grey-800 dark:text-grey-200">Notifications</h3>
-	</header>
-	{#if filteredNotifications.length > 0}
-		<div class="flex items-center">
-			<p class="text-sm font-medium text-grey-700 dark:text-grey-200">
-				{filteredNotifications.length} new notification{filteredNotifications.length > 1 ? 's' : ''}
-			</p>
+		{#if filteredNotifications.length > 0}
+			<div class="flex items-center">
+				<p class="text-sm font-medium text-grey-700 dark:text-grey-200">
+					{filteredNotifications.length} new notification{filteredNotifications.length > 1
+						? 's'
+						: ''}
+				</p>
 
-			<button
-				class="ml-auto text-sm font-medium text-grey-700 dark:text-grey-200"
-				on:click={handleClearNotifications}>Clear All</button
-			>
-		</div>
-	{/if}
+				<button
+					class="ml-auto text-sm font-medium text-grey-700 dark:text-grey-200"
+					on:click={handleClearNotifications}>Clear All</button
+				>
+			</div>
+		{/if}
+	</header>
+
 	<div class="mt-md flex flex-col gap-md">
 		{#each filteredNotifications as notification}
 			<Notification {...notification} />
