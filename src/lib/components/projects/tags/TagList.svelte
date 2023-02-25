23182e8b197b76290bfb186329bd6ebba261b66b
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-25 15:23:49 -0700

    Fixed tags not displaying properly sometimes

--- src/lib/components/projects/tags/TagList.svelte
+++ src/lib/components/projects/tags/TagList.svelte
@@ -2,16 +2,21 @@
 	import { onMount } from 'svelte';
 	import TagItem from './TagItem.svelte';
 
-	export let tags: string[];
+	export let tags: string | string[];
+	let formattedTags: string[] = [];
 	onMount(() => {
 		try {
-			tags = JSON.parse(tags.toString());
-		} catch {}
+			// tags = tags.toString().replace('', '');
+			formattedTags = JSON.parse(tags.toString());
+		} catch {
+			formattedTags = tags;
+		}
+		console.log(formattedTags);
 	});
 </script>
 
 <div class="mb-md flex flex-wrap gap-md">
-	{#each tags as tag}
+	{#each formattedTags as tag}
 		<TagItem {tag} />
 	{/each}
 </div>
