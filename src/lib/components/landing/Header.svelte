dbdf0a1578d3ba4e4c429f094e5aaae21f60555c
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-26 16:00:39 -0700

    Updated landing page header

---
+++ src/lib/components/landing/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+	import CloseMultiply from '$lib/assets/Close-Multiply.svelte';
+	import Menu from '$lib/assets/Menu.svelte';
+
+	let showMobileNav = false;
+</script>
+
+<header class="flex h-20 items-center bg-grey-100 py-lg px-lg">
+	<div class="flex items-center gap-md">
+		<div class="h-12 w-12 rounded-lg bg-grey-300" />
+		<span class="text-md font-semibold text-grey-800">Project Brew</span>
+	</div>
+	<nav class="ml-auto hidden items-center gap-md md:flex">
+		<a class="button--primary" href="/login">Log in</a>
+	</nav>
+
+	<div class="ml-auto block items-center md:hidden">
+		<button
+			on:click={() => (showMobileNav = !showMobileNav)}
+			class="absolute right-lg top-6 z-50"
+			aria-controls="landing page navigation"
+			aria-expanded={showMobileNav}
+		>
+			<span class="sr-only">Menu</span>
+			{#if showMobileNav}
+				<CloseMultiply className="stroke-grey-700 h-8 w-8" />
+			{:else}
+				<Menu className="stroke-grey-700 h-8 w-8" />
+			{/if}
+		</button>
+		<nav
+			class="fixed top-0 right-0 ml-auto flex h-screen w-7/12 flex-col items-start justify-end gap-md bg-grey-100 p-6 
+			{showMobileNav ? 'translate-x-0' : 'translate-x-full'} transition-all duration-300 ease-out"
+		>
+			<a class="button--primary w-full text-center" href="/login">Log in</a>
+		</nav>
+	</div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,12 @@
 <script>
-	import CloseMultiply from '$lib/assets/Close-Multiply.svelte';
-	import Menu from '$lib/assets/Menu.svelte';
-
-	let showMobileNav = false;
+	import Header from '$lib/components/landing/Header.svelte';
 </script>
 
 <svelte:head>
 	<title>Project Brew</title>
 </svelte:head>
 
-<header class="flex h-20 items-center bg-grey-100 py-lg px-lg">
-	<div class="flex items-center gap-md">
-		<div class="h-12 w-12 rounded-lg bg-grey-300" />
-		<span class="text-md font-semibold text-grey-800">Project Brew</span>
-	</div>
-	<nav class="ml-auto hidden items-center gap-md md:flex">
-		<a href="/" class="font-bold text-grey-700">About</a>
-		<a
-			href="https://github.com/nerbler09/project-brew"
-			target="__blank"
-			class="mr-md font-bold text-grey-700">Github</a
-		>
-		<a class="button--secondary" href="sign-up">Sign up</a>
-		<a class="button--primary" href="/login">Log in</a>
-	</nav>
-
-	<div class="ml-auto block items-center md:hidden">
-		<button
-			on:click={() => (showMobileNav = !showMobileNav)}
-			class="absolute right-lg top-6 z-50"
-			aria-controls="landing page navigation"
-			aria-expanded={showMobileNav}
-		>
-			<span class="sr-only">Menu</span>
-			{#if showMobileNav}
-				<CloseMultiply className="stroke-grey-700 h-8 w-8" />
-			{:else}
-				<Menu className="stroke-grey-700 h-8 w-8" />
-			{/if}
-		</button>
-		<nav
-			class="fixed top-0 right-0 ml-auto flex h-screen w-7/12 flex-col items-start justify-end gap-md bg-grey-100 p-6 
-			{showMobileNav ? 'translate-x-0' : 'translate-x-full'} transition-all duration-300 ease-out"
-		>
-			<a href="/" class="font-bold text-grey-700">About</a>
-			<a
-				href="https://github.com/nerbler09/project-brew"
-				target="__blank"
-				class="mb-auto font-bold text-grey-700">Github</a
-			>
-			<a class="button--secondary w-full text-center" href="sign-up">Sign up</a>
-			<a class="button--primary w-full text-center" href="/login">Log in</a>
-		</nav>
-	</div>
-</header>
+<Header />
 
 <section class="mb-md px-lg text-center">
 	<header class="mt-md md:mt-lg">
