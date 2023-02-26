70d8471a555fae6cd40ff591795fd18d74672540
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-02-26 15:57:17 -0700

    Downloaded assets for landing page redesign

---
+++ src/lib/assets/Landing-Page/CreditCard.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let className = '';
+</script>
+
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class={className}>
+	<path
+		d="M3.75 16.75V7.25C3.75 6.14543 4.64543 5.25 5.75 5.25H18.25C19.3546 5.25 20.25 6.14543 20.25 7.25V16.75C20.25 17.8546 19.3546 18.75 18.25 18.75H5.75C4.64543 18.75 3.75 17.8546 3.75 16.75Z"
+		stroke-width="1.5"
+	/>
+	<path d="M3.75 9.25H20.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+	<path
+		d="M13.25 15.75L17.25 15.75"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
