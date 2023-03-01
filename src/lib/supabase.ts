fae3cef15150fade4eb76ff479132a9c3d224f3e
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-01 07:15:33 -0700

    Fixed minor supabase error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,15 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 
-let supabase = createClient(import.meta.env.VITE_PUBLIC_SUPABSE_URL, import.meta.env.VITE_PUBLIC_SUPABSE_ANON_KEY, {
-  realtime: {
-    params: {
-      eventsPerSecond: 10
-    }
-  }
-});
+const supabase = createClient(
+	import.meta.env.VITE_PUBLIC_SUPABSE_URL,
+	import.meta.env.VITE_PUBLIC_SUPABSE_ANON_KEY,
+	{
+		realtime: {
+			params: {
+				eventsPerSecond: 10
+			}
+		}
+	}
+);
 
 export { supabase };
