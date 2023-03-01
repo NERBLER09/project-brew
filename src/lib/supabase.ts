8366d9359521992b72e039bc539de08e27c6c0cc
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-01 07:24:03 -0700

    Fixed spelling mistake

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 
 const supabase = createClient(
-	import.meta.env.VITE_PUBLIC_SUPABSE_URL,
-	import.meta.env.VITE_PUBLIC_SUPABSE_ANON_KEY,
+	import.meta.env.VITE_PUBLIC_SUPABASE_URL,
+	import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
 	{
 		realtime: {
 			params: {
