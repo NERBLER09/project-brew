b1c7852ec80f49942b46de37e05b22aa795a02b7
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-03 14:23:54 -0700

    Fixed settings header text being not visible on light mode

--- src/lib/components/settings/header/Header.svelte
+++ src/lib/components/settings/header/Header.svelte
@@ -15,8 +15,8 @@
 		? 'linear-gradient(180deg, rgba(0, 0, 0, 0) 35.95%, rgba(0, 0, 0, 0.6) 100%),'
 		: ''} url({data.banner}) ;"
 	class="{!data.banner
-		? 'h-fit static my-6 md:my-8'
-		: 'h-[15.625rem] relative '} flex items-end rounded-b-3xl bg-cover bg-center bg-no-repeat object-cover px-6 md:px-8"
+		? 'static my-6 h-fit md:my-8'
+		: 'relative h-[15.625rem] '} flex items-end rounded-b-3xl bg-cover bg-center bg-no-repeat object-cover px-6 md:px-8"
 >
 	<div
 		class="{!data.banner
@@ -36,7 +36,13 @@
 		{/if}
 
 		<div class="z-50 mt-sm flex h-full w-full flex-col overflow-hidden md:mt-md md:w-auto">
-			<h1 class="text-lg text-grey-100 md:text-xl">{$settingsPage}</h1>
+			<h1
+				class="text-lg  md:text-xl {data.banner
+					? 'text-grey-100'
+					: 'text-grey-700 dark:text-grey-100'}"
+			>
+				{$settingsPage}
+			</h1>
 			<div class="flex w-full gap-md md:hidden">
 				<div class="flex w-3/4 flex-col">
 					<span class="inline-block truncate font-bold text-grey-700 dark:text-grey-200 md:hidden"
