f8ff72118c07b44088f28959d142ffd4f0482407
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-05 14:52:10 -0700

    Updated how new team buttons looked

---
+++ src/lib/components/dropdowns/team/JoinCreateTeam.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Edit from '$lib/assets/Edit.svelte';
+	import UserAdd from '$lib/assets/User-Add.svelte';
+</script>
+
+<!-- Only shows on mobile -->
+<div class="dropdown--container">
+	<span class="dropdown--item">
+		<Edit className="dropdown--icon" />
+		<span class="dropdown--label">Create team</span>
+	</span>
+	<span class="dropdown--item">
+		<UserAdd className="dropdown--icon" />
+		<span class="dropdown--label">Create team</span>
+	</span>
+</div>
--- src/routes/app/team/+page.svelte
+++ src/routes/app/team/+page.svelte
@@ -1,44 +1,53 @@
 <script lang="ts">
 	import UserAdd from '$lib/assets/User-Add.svelte';
+	import JoinCreateTeam from '$lib/components/dropdowns/team/JoinCreateTeam.svelte';
 	import InviteMember from '$lib/components/prompts/team/InviteMember.svelte';
-
-	import TeamMemberLink from '$lib/components/team/TeamMemberLink.svelte';
 	import type { PageData } from './$types';
 
-	export let data: PageData;
-
 	let showInvitePrompt = false;
+
+	let mobileAddContainer: HTMLElement;
+	let showAddTeamDropdown = false;
+	const handleAutoCloseDropdown = (event: Event) => {
+		if (!mobileAddContainer.contains(event.target)) {
+			showAddTeamDropdown = false;
+		}
+	};
 </script>
+
+<svelte:window on:click={handleAutoCloseDropdown} />
 
 <header>
 	<div class="flex items-center">
-		<h1 class="text-lg text-grey-800 dark:text-grey-100 md:text-2xl">Team Members</h1>
-		<button
-			class="button--primary ml-auto hidden items-center gap-md md:flex"
-			on:click={() => (showInvitePrompt = true)}
-		>
-			<UserAdd className="h-8 w-8 stroke-grey-200" />
-			<span>Invite</span>
-		</button>
+		<h1 class="text-lg text-grey-800 dark:text-grey-100 md:text-2xl">Team</h1>
+		<div class="ml-auto hidden gap-md md:flex">
+			<button class="button--secondary">Join Team</button>
+			<button
+				class="button--primary hidden items-center gap-md md:flex"
+				on:click={() => (showInvitePrompt = true)}
+			>
+				<UserAdd className="h-6 w-6 stroke-grey-200" />
+				<span>New Team</span>
+			</button>
+		</div>
 	</div>
+
 	<p class="pt-md font-medium text-grey-700 dark:text-grey-200 md:pt-sm">
-		Tap on a member to view their details.
+		Select a team to view it.
 	</p>
 
-	<a class="button--circle absolute bottom-32 right-8 z-50 md:hidden" href="/app/team/add">
-		<UserAdd className="h-8 w-8 stroke-grey-200" />
-		<span class="sr-only">Invite a new team member</span>
-	</a>
+	<div bind:this={mobileAddContainer}>
+		<button
+			class="button--circle absolute bottom-32 right-8 z-50 md:hidden"
+			on:click={() => (showAddTeamDropdown = true)}
+		>
+			<UserAdd className="h-8 w-8 stroke-grey-200" />
+			<span class="sr-only">Invite a new team member</span>
+		</button>
+		{#if showAddTeamDropdown}
+			<JoinCreateTeam />
+		{/if}
+	</div>
 </header>
 
-<div class="mt-md flex w-full flex-col items-start gap-lg lg:grid lg:grid-cols-2 xl:grid-cols-4">
-	{#each data.members as id}
-		<TeamMemberLink {id} />
-	{:else}
-		<p class="font-medium text-grey-700 dark:text-grey-200">
-			People you have added to your team will appear here. To add a person click on the "invite"
-			button.
-		</p>
-	{/each}
-</div>
 <InviteMember bind:shown={showInvitePrompt} />
