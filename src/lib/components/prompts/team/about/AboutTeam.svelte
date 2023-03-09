3791b02aaa2ab94c0445d0be864759ef2364a4bf
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-09 06:50:08 -0700

    Allow user to update description on desktop

--- src/lib/components/prompts/team/about/AboutTeam.svelte
+++ src/lib/components/prompts/team/about/AboutTeam.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
 	import CloseMultiply from '$lib/assets/Close-Multiply.svelte';
 	import TeamMember from '$lib/components/team/about/TeamMember.svelte';
 	import { currentTeam } from '$lib/stores/team';
+	import { supabase } from '$lib/supabase';
 	import type { TeamMembers } from '$lib/types/projects';
+	import toast from 'svelte-french-toast';
 
 	export let shown = false;
 	export let teamMembers: TeamMembers[];
 	let dialog: HTMLDialogElement;
+
+	let description = $currentTeam.description;
+
+	const handleUpdateDescription = async () => {
+		if (description === $currentTeam.description) return;
+		const { error } = await supabase
+			.from('teams')
+			.update({ description })
+			.eq('id', $currentTeam.id);
+		if (!error) {
+			invalidate('app:team');
+			$currentTeam.description = description;
+		} else {
+			toast.error(`Failed to update team description: ${error.message}`);
+		}
+	};
 
 	const handleModalStatus = (status: boolean) => {
 		if (!dialog) return;
@@ -53,7 +72,14 @@
 	</header>
 
 	<div class={$currentTeam.banner ? 'relative -top-6' : ''}>
-		<p class="my-md font-medium text-grey-700 dark:text-grey-300">{$currentTeam.description}</p>
+		<p
+			class="my-md font-medium text-grey-700 dark:text-grey-300"
+			contenteditable="true"
+			bind:textContent={description}
+			on:blur={handleUpdateDescription}
+		>
+			{$currentTeam.description}
+		</p>
 		<section class="mt-md">
 			<header>
 				<h2 class="text-md font-semibold text-grey-700 dark:text-grey-200">Invited team members</h2>
