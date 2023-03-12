461468504c07f828212a0f7e98adf6d9bd2627c4
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-12 10:18:37 -0600

    Prevent viewer on mobile changing details

--- src/routes/app/team/[slug]/about/+page.svelte
+++ src/routes/app/team/[slug]/about/+page.svelte
@@ -16,6 +16,7 @@
 
 	let description = data.team.description;
 	let name = data.team.name;
+	const isViewer = data.role === 'viewer';
 
 	const handleUpdateDescription = async () => {
 		if (description === data.team.description) return;
@@ -68,35 +69,53 @@
 					: 'stroke-grey-700 dark:stroke-grey-200'}"
 			/>
 		</a>
-		<h1
-			class="text-lg {data.team.banner
-				? 'max-w-[calc(100%-80px)] text-grey-200'
-				: 'w-full text-grey-700 dark:text-grey-200'} truncate"
-			contenteditable="true"
-			bind:textContent={name}
-			on:blur={handleUpdateName}
-		>
-			{data.team.name}
-		</h1>
+		{#if !isViewer}
+			<h1
+				class="text-lg {data.team.banner
+					? 'max-w-[calc(100%-80px)] text-grey-200'
+					: 'w-full text-grey-700 dark:text-grey-200'} truncate"
+				contenteditable="true"
+				bind:textContent={name}
+				on:blur={handleUpdateName}
+			>
+				{data.team.name}
+			</h1>
+		{:else}
+			<h1
+				class="text-lg {data.team.banner
+					? 'max-w-[calc(100%-80px)] text-grey-200'
+					: 'w-full text-grey-700 dark:text-grey-200'} truncate"
+			>
+				{data.team.name}
+			</h1>
+		{/if}
 	</div>
 </header>
 
-<button
-	class="button--secondary flex w-full items-center justify-center gap-md"
-	on:click={() => (showUpdateBannerPrompt = true)}
->
-	<Image className="w-6 h-6 stroke-grey-700 dark:stroke-grey-200" />
-	Modify Banner
-</button>
+{#if !isViewer}
+	<button
+		class="button--secondary mt-md flex w-full items-center justify-center gap-md"
+		on:click={() => (showUpdateBannerPrompt = true)}
+	>
+		<Image className="w-6 h-6 stroke-grey-700 dark:stroke-grey-200" />
+		Modify Banner
+	</button>
+{/if}
 
-<p
-	class="my-md font-medium text-grey-700 dark:text-grey-300"
-	contenteditable="true"
-	bind:textContent={description}
-	on:blur={handleUpdateDescription}
->
-	{data.team.description}
-</p>
+{#if !isViewer}
+	<p
+		class="my-md font-medium text-grey-700 dark:text-grey-300"
+		contenteditable="true"
+		bind:textContent={description}
+		on:blur={handleUpdateDescription}
+	>
+		{data.team.description}
+	</p>
+{:else}
+	<p class="my-md font-medium text-grey-700 dark:text-grey-300">
+		{data.team.description}
+	</p>
+{/if}
 
 <form
 	action="?/invite"
