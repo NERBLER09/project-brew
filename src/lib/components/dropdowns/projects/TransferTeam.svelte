d7c46a1a6a75bfab19848dc528656d7476a7f33f
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-12 14:46:09 -0600

    Allow user to be able to remove a project from a team

--- src/lib/components/dropdowns/projects/TransferTeam.svelte
+++ src/lib/components/dropdowns/projects/TransferTeam.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Trash from '$lib/assets/Trash.svelte';
 	import { currentProject } from '$lib/stores/project';
 	import { userData } from '$lib/stores/user';
 	import { supabase } from '$lib/supabase';
@@ -8,7 +9,7 @@
 
 	export let visibility: boolean;
 
-	let teams: Teams = [];
+	let teams: Teams[] = [];
 
 	onMount(async () => {
 		const { data } = await supabase
@@ -30,18 +31,32 @@
 			visibility = false;
 		} else toast.error(`Failed to tranfer team: ${error.message}`);
 	};
+
+	const handleRemoveTeam = async () => {
+		const { error } = await supabase
+			.from('projects')
+			.update({ team: null })
+			.eq('id', $currentProject.id);
+
+		if (!error) {
+			$currentProject.team = null;
+			toast.success('Removed from team');
+			visibility = false;
+		} else toast.error(`Failed to remove from team: ${error.message}`);
+	};
 </script>
 
 <div
 	class="absolute left-0 top-6 flex h-fit w-screen max-w-[calc(100vw-3rem)] flex-col gap-4 rounded-2xl bg-grey-100 px-6 py-lg text-start dark:bg-grey-800 md:w-96 md:p-md"
 >
-	<div class="dropdown--item">
-		<span class="dropdown--label">Transfer this project to another team</span>
-	</div>
+	<button class="dropdown--item" on:click={handleRemoveTeam}>
+		<Trash className="dropdown--icon" />
+		<span class="dropdown--label">Remove from this team</span>
+	</button>
 
 	{#each teams as team}
 		<button on:click={() => handleUpdateTeam(team.id)} class="dropdown--item"
-			><span class="dropdown--label">{team.name}</span></button
+			><span class="dropdown--label">Move to {team.name}</span></button
 		>
 	{/each}
 </div>
