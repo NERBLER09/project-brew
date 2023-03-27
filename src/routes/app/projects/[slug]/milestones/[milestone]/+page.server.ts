ebf84cbe0eeeafc3327b9d6bd5918c4af976bb72
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-27 15:23:44 -0600

    Created basic milestone page

---
+++ src/routes/app/projects/[slug]/milestones/[milestone]/+page.server.ts
@@ -0,0 +1,24 @@
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { error, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load = (async (event) => {
+  const { session, supabaseClient } = await getSupabase(event);
+  if (!session) {
+    throw redirect(303, '/');
+  }
+
+  const milestoneId = event.params.milestone;
+
+  const { data: milestone, error: err } = await supabaseClient
+    .from('milestones')
+    .select()
+    .eq('id', milestoneId)
+    .limit(1)
+    .single();
+
+  if (!err) {
+    return { milestone };
+  }
+  throw error(404, `Failed to fetch milestone ${err.message}`);
+}) satisfies PageServerLoad;
---
+++ src/routes/app/projects/[slug]/milestones/[milestone]/+page@app.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import MobileSubPageLayout from '$lib/components/layouts/MobileSubPageLayout.svelte';
+	import { currentProject } from '$lib/stores/project';
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>{data.milestone.name}</title>
+</svelte:head>
+
+<MobileSubPageLayout
+	pageName={data.milestone.name}
+	previousPage="/app/projects/{$currentProject.id}/milestones"
+>
+	<header class="hidden md:inline">
+		<h1 class="text-2xl font-semibold text-grey-900 dark:text-grey-100">{data.milestone.name}</h1>
+	</header>
+</MobileSubPageLayout>
