743810595927755668194a18a26c85732a4885ab
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-31 14:54:45 -0600

    Display a milestone task sub task progress

--- src/lib/components/projects/milestones/MilestoneTask.svelte
+++ src/lib/components/projects/milestones/MilestoneTask.svelte
@@ -10,11 +10,33 @@
 
 	let assignedUserProfiles: any[] = [];
 
+	interface SubTask {
+		completed: boolean;
+		id: string;
+		list: number;
+		name: string;
+		project: number;
+		task: number;
+	}
+
+	let subTasks: SubTask[] = [];
+	let total = 0;
+	let completed = 0;
+
+	const getSubTasks = async () => {
+		console.log(id);
+		const { data: tasks } = await supabase.from('sub_tasks').select().eq('task', id);
+		subTasks = tasks ?? [];
+		total = subTasks.length;
+		completed = [...subTasks.filter((item) => item.completed)].length;
+	};
+
 	onMount(async () => {
 		if (!assignedUserProfiles) return;
 		const { data } = await supabase.from('profiles').select('avatar_url, name').in('id', assigned);
 		assignedUserProfiles =
 			data?.map((item) => ({ profile: item.avatar_url ?? '', name: item.name })) ?? [];
+		getSubTasks();
 	});
 </script>
 
@@ -24,6 +46,21 @@
 			? 'text-emerald-600 line-through dark:text-emerald-500'
 			: ''}">{name}</span
 	>
+
+	{#if subTasks.length > 0}
+		<div class="ml-2xl hidden w-full max-w-[18.75rem] items-center gap-md md:flex">
+			<span class="font-bold text-grey-700 dark:text-grey-300">{completed}/{total}</span>
+			<span class="sr-only">{completed}/{total} of this task's sub tasks are completed</span>
+
+			<div class="relative w-full">
+				<div
+					class="absolute h-1 rounded-full bg-grey-700 dark:bg-grey-300"
+					style="width: {(completed / total) * 100}%"
+				/>
+				<div class="h-1 w-full rounded-full bg-grey-300 dark:bg-grey-700" />
+			</div>
+		</div>
+	{/if}
 
 	<div class="ml-auto flex w-fit flex-row-reverse items-center">
 		{#each assignedUserProfiles as assigned}
