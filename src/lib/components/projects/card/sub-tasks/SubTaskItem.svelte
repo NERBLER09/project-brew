063ea7a5b977da555d1db2ba4afe06ca60f197a1
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-03-31 12:12:31 -0600

    Allow user to be able to update sub task status

--- src/lib/components/projects/card/sub-tasks/SubTaskItem.svelte
+++ src/lib/components/projects/card/sub-tasks/SubTaskItem.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+	import { supabase } from '$lib/supabase';
+
 	export let completed: boolean;
 	export let id: string;
 	export let name: string;
 	export let task: number;
+
+	const updateStatus = async () => {
+		await supabase.from('sub_tasks').update({ completed }).eq('id', id);
+	};
 </script>
 
 <div>
-	<input type="checkbox" class="input--checkbox" bind:checked={completed} id="{name}-sub-task" />
+	<input
+		type="checkbox"
+		class="input--checkbox"
+		bind:checked={completed}
+		id="{name}-sub-task"
+		on:change={updateStatus}
+	/>
 	<label for="{name}-sub-task" class="input--label">{name}</label>
 </div>
