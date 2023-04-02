831e10b4a431175f21b1257417a847df3b0a8216
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-04-02 11:39:52 -0600

    Display progress wheel for today's progress in project dashboard

---
+++ src/routes/app/projects/[slug]/dashboard/+page.server.ts
@@ -0,0 +1,36 @@
+import type { PageServerLoad } from './$types';
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { error, redirect } from '@sveltejs/kit';
+
+export const load = (async (event) => {
+  const { session, supabaseClient } = await getSupabase(event);
+  if (!session) {
+    throw redirect(303, '/');
+  }
+
+  const params = event.params;
+  const projectId = params.slug;
+
+  event.depends('project:dashboard');
+
+  const { data: tasks, error: err } = await supabaseClient
+    .from('tasks')
+    .select()
+    .eq('project', projectId);
+
+  const { data: today } = await supabaseClient
+    .from('tasks')
+    .select()
+    .eq('due_date', new Date().toISOString())
+    .eq('project', projectId);
+
+  if (tasks) {
+    return {
+      dashboard: {
+        tasks,
+        today
+      }
+    };
+  }
+  return error(400, err?.message);
+}) satisfies PageServerLoad;
--- src/routes/app/projects/[slug]/dashboard/+page.svelte
+++ src/routes/app/projects/[slug]/dashboard/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { showProjectAside } from '$lib/stores/project';
 	import { onDestroy, onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let strokeArray = 380;
+	let totalAmountOfTasks = data.dashboard?.today?.length ?? 0;
+	let completedTasks = data.dashboard?.today?.filter((item) => item.status === 'done').length ?? 0;
+	let percentCompleted = (completedTasks / totalAmountOfTasks) * 100;
 
 	onMount(() => {
 		$showProjectAside = false;
@@ -13,9 +21,38 @@
 
 <div class="grid gap-md md:grid-cols-2 md:gap-lg lg:grid-cols-5 lg:gap-2xl">
 	<section class="md:col-span-2 lg:col-span-3">
-		<header>
+		<header class="mb-sm">
 			<h2 class="text-md font-semibold text-grey-800 dark:text-grey-200">Today's Progress</h2>
 		</header>
+
+		<div
+			class="relative flex aspect-square h-32 w-32 flex-col items-center justify-center gap-sm rounded-full"
+		>
+			<div class="absolute">
+				<svg class="h-32 w-32">
+					<circle
+						cx="64"
+						cy="64"
+						class="fill-none stroke-grey-200 dark:stroke-grey-700"
+						stroke-width="8"
+						r="60"
+					/>
+					<circle
+						cx="64"
+						cy="64"
+						r="60"
+						class="fill-none stroke-accent-light transition-all duration-300 ease-in"
+						stroke-dashoffset={strokeArray - (strokeArray * percentCompleted) / 100}
+						stroke-dasharray={strokeArray}
+						stroke-linecap="round"
+						stroke-width="8"
+						transform="rotate(-90 64 64)"
+					/>
+				</svg>
+			</div>
+			<span class="font-bold text-grey-700 dark:text-grey-200">{percentCompleted}%</span>
+			<span class="font-medium text-grey-700 dark:text-grey-200">Completed</span>
+		</div>
 	</section>
 	<section class="md:col-span-1 md:row-start-3 lg:col-span-2 lg:col-start-4 lg:row-start-1">
 		<header>
