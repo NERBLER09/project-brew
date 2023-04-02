603138d4c2cc69f6aa03723b3d9caa73ff92acb4
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-04-02 10:51:29 -0600

    Setup basic layout for projet dashboard

--- src/routes/app/projects/[slug]/dashboard/+page.svelte
+++ src/routes/app/projects/[slug]/dashboard/+page.svelte
@@ -10,3 +10,30 @@
 		$showProjectAside = true;
 	});
 </script>
+
+<div class="grid gap-md md:grid-cols-2 md:gap-lg lg:grid-cols-5 lg:gap-2xl">
+	<section class="md:col-span-2 lg:col-span-3">
+		<header>
+			<h2 class="text-md font-semibold text-grey-800 dark:text-grey-200">Today's Progress</h2>
+		</header>
+	</section>
+	<section class="md:col-span-1 md:row-start-3 lg:col-span-2 lg:col-start-4 lg:row-start-1">
+		<header>
+			<h2 class="text-md font-semibold text-grey-800 dark:text-grey-200">Behind Tasks</h2>
+		</header>
+	</section>
+	<section
+		class="row-start-4 md:col-span-1 md:row-start-3 lg:col-span-2 lg:col-start-1 lg:row-start-2"
+	>
+		<header>
+			<h2 class="text-md font-semibold text-grey-800 dark:text-grey-200">Task Workload</h2>
+		</header>
+	</section>
+	<section
+		class="row-start-3 md:col-span-2 md:row-start-2 lg:col-span-3 lg:col-start-3 lg:row-start-2"
+	>
+		<header>
+			<h2 class="text-md font-semibold text-grey-800 dark:text-grey-200">Milestone Progress</h2>
+		</header>
+	</section>
+</div>
