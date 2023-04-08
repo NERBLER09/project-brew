99c34551b799378c9539e7e27290882edf3cd3f6
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-04-08 17:34:21 -0600

    Created basic layout for project table page

---
+++ src/routes/app/projects/[slug]/list/+page.server.ts
@@ -0,0 +1,25 @@
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { error, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load = (async (event) => {
+	const { session, supabaseClient } = await getSupabase(event);
+	if (!session) {
+		throw redirect(303, '/');
+	}
+
+	const params = event.params;
+	const projectId = params.slug;
+
+	event.depends('project:board');
+
+	const { data: tasks } = await supabaseClient.from('tasks').select().eq('project', projectId);
+
+	if (tasks) {
+		return {
+			tasks: tasks || []
+		};
+	}
+
+	return error(404, 'Failed to fetch project board lists');
+}) satisfies PageServerLoad;
