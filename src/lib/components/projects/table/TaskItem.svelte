c1cefbd5ebb8c9f9672f3eb8b6e5674019d3d8e6
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-04-22 20:50:03 -0600

    Allow user to be able to update task priority

--- src/lib/components/projects/table/TaskItem.svelte
+++ src/lib/components/projects/table/TaskItem.svelte
@@ -5,6 +5,7 @@
 	import { supabase } from '$lib/supabase';
 	import { onMount } from 'svelte';
 	import Assinged from '../card/Assinged.svelte';
+	import PriorityLevel from '../card/priority/PriorityLevel.svelte';
 
 	export let name: string;
 	export let tags: string[] = [];
@@ -107,27 +108,7 @@
 	</div>
 	<div class="mr-md min-w-[10.625rem]">
 		<div class="w-fit">
-			{#if priority_level === 'high'}
-				<div class="rounded-full bg-red-400 py-1 px-4 dark:bg-red-600">
-					<span class="sr-only">High priority</span>
-					<span class="font-medium text-grey-100">High</span>
-				</div>
-			{:else if priority_level === 'med'}
-				<div class="rounded-full bg-orange-400 py-1 px-4 dark:bg-orange-600">
-					<span class="sr-only">Medium priority</span>
-					<span class="font-medium text-grey-100">Medium</span>
-				</div>
-			{:else if priority_level === 'low'}
-				<div class="rounded-full bg-yellow-300 py-1 px-4 dark:bg-yellow-500">
-					<span class="sr-only">Low priority</span>
-					<span class="font-medium text-grey-700">Low</span>
-				</div>
-			{:else if priority_level === 'none' || priority_level === null}
-				<div class="rounded-full bg-emerald-400 py-1 px-4 dark:bg-emerald-500">
-					<span class="sr-only">No priority</span>
-					<span class="font-medium text-grey-100">None</span>
-				</div>
-			{/if}
+			<PriorityLevel {priority_level} taskId={id} />
 		</div>
 	</div>
 	<div class="relative mr-md min-w-[10.625rem]">
