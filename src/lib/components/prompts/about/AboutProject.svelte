0f2eb9252fafbe5a2206809b5a2648c8a058c62c
Finnegan Beaudin <noah.beaudin1@gmail.com>
2023-09-24 20:05:56 -0600

    Updated about project prompt to be editable without entering an edit mode

--- src/lib/components/prompts/about/AboutProject.svelte
+++ src/lib/components/prompts/about/AboutProject.svelte
@@ -2,26 +2,21 @@
 	import { invalidate } from '$app/navigation';
 
 	import CloseMultiply from '$lib/assets/Close-Multiply.svelte';
-	import Edit from '$lib/assets/Edit.svelte';
-	import Image from '$lib/assets/Image.svelte';
-	import Trash from '$lib/assets/Trash.svelte';
 	import InvitedTeamMembers from '$lib/components/projects/about/InvitedTeamMembers.svelte';
 	import TransferProjectToTeam from '$lib/components/projects/about/TransferProjectToTeam.svelte';
-	import NewTagsInput from '$lib/components/projects/edit/NewTagsInput.svelte';
-	import TagList from '$lib/components/projects/tags/TagList.svelte';
 
 	import { currentProject, userTeams } from '$lib/stores/project';
 	import { showManageInvitedPrompt } from '$lib/stores/ui';
 	import { supabase } from '$lib/supabase';
-	import lodash from 'lodash';
 	import { onMount } from 'svelte';
 	import toast from 'svelte-french-toast';
 	import ManageInvited from './ManageInvited.svelte';
+	import FileInput from '$lib/components/form/FileInput.svelte';
 
 	export let shown = false;
 	let dialog: HTMLDialogElement;
 
-	export let project_name: string | null = '';
+	export let project_name: string | null;
 	export let description: string | null = '';
 	export let tags: string[] | null = [];
 	export let banner: string | null = '';
@@ -41,12 +36,8 @@
 	$: handleModalStatus(shown);
 
 	let inEditMode = false;
-	let newProjectName = project_name;
-	let newProjectDescription = description;
-	let newProjectTags: string[] = tags;
-	let newCoverURL = banner;
-	let newCoverFile: FileList | null;
-	let coverInputElement: HTMLInputElement;
+	let bannerURL = banner;
+	let newBanner: FileList | null;
 
 	let teamName = '';
 
@@ -68,82 +59,88 @@
 		getTeamName();
 	});
 
-	const uploadNewCover = async (coverList: FileList | null) => {
-		if (!coverList) return '';
-		let cover = coverList[0];
+	const updateProjectName = async () => {
+		const { error } = await supabase
+			.from('projects')
+			.update({
+				project_name
+			})
+			.eq('id', $currentProject.id);
 
-		const ccName = lodash.camelCase(newProjectName ?? '');
+		if (!error) {
+			invalidate('app:project');
+			invalidate('project:about');
+			toast.success('Updated project details');
+		} else {
+			toast.error(`Failed to update project details: ${error?.message}`);
+		}
+	};
+
+	const updateProjectDescription = async () => {
+		const { error } = await supabase
+			.from('projects')
+			.update({
+				description
+			})
+			.eq('id', $currentProject.id);
+
+		if (!error) {
+			invalidate('app:project');
+			invalidate('project:about');
+			toast.success('Updated project details');
+		} else {
+			toast.error(`Failed to update project details: ${error?.message}`);
+		}
+	};
+
+	const updateProjectBanner = async () => {
 		const { data: user } = await supabase.auth.getUser();
-
-		const { error: coverErr } = await supabase.storage
-			.from('project-covers')
-			.upload(`${user.user?.id}/${ccName}.png`, cover, {
+		if (newBanner.size !== 0 && newBanner.size < 5000000) {
+			await supabase.storage.from('avatars').upload(`${user?.user.id}/cover.png`, newBanner, {
 				cacheControl: '3600',
 				upsert: true
 			});
 
-		if (!coverErr) {
 			const { data: url } = supabase.storage
-				.from('project-covers')
-				.getPublicUrl(`${user.user?.id}/${ccName}.png`);
-
-			return url.publicUrl;
-		} else {
-			toast.error('Failed to upload project banner. Try again');
+				.from('avatars')
+				.getPublicUrl(`${user?.user.id}/cover.png`);
+			bannerURL = url.publicUrl;
+		} else if (newBanner.size > 5000000 && newBanner.size > 0) {
+			toast.error("Project banner can't be larger then 5mb");
 		}
-	};
 
-	const handleUpdateProject = async () => {
-		let updatedCoverURL = $currentProject.banner;
-		if (newCoverFile && newCoverFile[0].size > 5000000) {
-			toast.error("Cover can't be over 5mb in size.");
-			return;
-		} else if (newCoverFile && newCoverFile[0].size < 5000000) {
-			updatedCoverURL = await uploadNewCover(newCoverFile);
-		}
-		inEditMode = false;
-		const { data, error } = await supabase
+		const { error } = await supabase
 			.from('projects')
 			.update({
-				project_name: newProjectName ?? '',
-				description: newProjectDescription,
-				tags: JSON.parse(newProjectTags),
-				banner: updatedCoverURL
+				banner: bannerURL
 			})
-			.eq('id', $currentProject.id)
-			.select();
+			.eq('id', $currentProject.id);
 
-		if (data) {
-			$currentProject = data[0];
-			$currentProject.tags = JSON.parse(data[0]?.tags ?? '[]');
-			$currentProject.project_name = data[0]?.project_name;
-			console.log(data[0].tags);
+		if (!error) {
 			invalidate('app:project');
+			invalidate('project:about');
 			toast.success('Updated project details');
 		} else {
-			toast.error(`Failed to update project details: ${error.message}`);
+			toast.error(`Failed to update project details: ${error?.message}`);
 		}
 	};
 
-	const handleRemoveCover = () => {
-		newCoverURL = null;
-		newCoverFile = null;
+	const removeBanner = async () => {
+		const { error } = await supabase
+			.from('projects')
+			.update({
+				banner: null
+			})
+			.eq('id', $currentProject.id);
+
+		if (!error) {
+			invalidate('app:project');
+			invalidate('project:about');
+			toast.success('Updated project details');
+		} else {
+			toast.error(`Failed to update project details: ${error?.message}`);
+		}
 	};
-
-	const getFileURL = (file: any) => {
-		if (!file) return;
-		newCoverURL = URL.createObjectURL(file);
-	};
-
-	$: if (newCoverFile) getFileURL(newCoverFile[0]);
-
-	$: if (!inEditMode) {
-		newProjectName = project_name;
-		newProjectDescription = description;
-		newProjectTags = tags ?? [];
-		newCoverURL = $currentProject.banner;
-		newCoverFile = null;
-	}
 </script>
 
 <dialog
@@ -152,57 +149,28 @@
 	on:close={() => (shown = false)}
 >
 	<header
-		class="relative -left-8 -top-8 flex w-[calc(100%+64px)] items-end rounded-b-3xl bg-cover bg-center object-cover p-6 font-semibold {!newCoverURL
+		class="relative -left-8 -top-8 flex w-[calc(100%+64px)] items-end rounded-b-3xl bg-cover bg-center object-cover p-6 font-semibold {!bannerURL
 			? 'w-[calc(100%+64px)]'
 			: 'h-[12.5rem]'}"
-		style="background-image: {newCoverURL
+		style="background-image: {bannerURL
 			? 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.6) 115.18%),'
-			: ''} url({!inEditMode ? $currentProject.banner : newCoverURL});"
+			: ''} url({bannerURL});"
 	>
-		{#if !inEditMode}
-			<h1
-				class="w-fit text-lg {banner
-					? 'text-grey-200'
-					: 'text-grey-700 dark:text-grey-200'} truncate"
-			>
-				{project_name}
-			</h1>
-		{:else}
-			<h1
-				class="w-fit text-lg {newCoverURL
-					? 'text-grey-200'
-					: 'text-grey-700 dark:text-grey-200'} truncate"
-				contenteditable="true"
-				bind:textContent={newProjectName}
-			>
-				{$currentProject?.project_name}
-			</h1>
-		{/if}
+		<h1
+			class="w-fit text-lg {bannerURL
+				? 'text-grey-200'
+				: 'text-grey-700 dark:text-grey-200'} truncate"
+			contenteditable
+			bind:innerHTML={project_name}
+			on:blur={updateProjectName}
+		>
+			{project_name}
+		</h1>
 
 		<div class="relative mb-auto ml-auto flex items-center gap-md">
-			{#if inEditMode}
-				<button on:click={() => (inEditMode = false)}>
-					<Trash
-						className="h-8 w-8 {newCoverURL
-							? 'stroke-grey-200'
-							: 'stroke-grey-700 dark:stroke-grey-200'}"
-					/>
-					<span class="sr-only">Discard changes</span>
-				</button>
-			{:else}
-				<button on:click={() => (inEditMode = true)}>
-					<Edit
-						className="h-8 w-8 {$currentProject.banner
-							? 'stroke-grey-200'
-							: 'stroke-grey-700 dark:stroke-grey-200'}"
-					/>
-					<span class="sr-only">Edit project details</span>
-				</button>
-			{/if}
-
 			<button on:click={() => (shown = false)}>
 				<CloseMultiply
-					className=" {newCoverURL
+					className=" {bannerURL
 						? 'stroke-grey-200'
 						: 'stroke-grey-700 dark:stroke-grey-200'} w-12 h-12"
 				/>
@@ -212,89 +180,43 @@
 	</header>
 
 	<div class="relative {$currentProject.banner ? '-top-6' : '-top-8'}">
-		<div class="flex flex-wrap">
-			{#if tags}
-				<div class="mb-sm flex flex-wrap gap-md">
-					{#if inEditMode}
-						<NewTagsInput bind:newTags={newProjectTags} />
-					{:else}
-						<TagList tags={$currentProject.tags} />
-					{/if}
-				</div>
-			{/if}
-		</div>
-
-		{#if !inEditMode}
-			{#if $currentProject.description}
-				<p class="my-sm text-sm font-medium text-grey-700 dark:text-grey-300">
-					{$currentProject.description}
-				</p>
-			{/if}
-
-			{#if $userTeams?.length > 0}
-				<TransferProjectToTeam
-					{teamName}
-					projectId={$currentProject.id}
-					team={$currentProject.team}
-				/>
-			{/if}
-			<InvitedTeamMembers
-				invited_people={$currentProject.invited_people}
-				projectId={$currentProject.id}
-			/>
-		{:else}
-			<div class="flex flex-col">
-				<label for="description-input" class="input--label mb-sm"
-					>Edit the project description</label
-				>
-				<textarea
-					name="description"
-					class="input--text h-36 w-full resize-none"
-					placeholder="Enter a brief description"
-					bind:value={newProjectDescription}
-				/>
-			</div>
+		<!-- TODO: Add proper input for tags -->
+		{#if $currentProject.description}
+			<p
+				class="my-sm text-sm font-medium text-grey-700 dark:text-grey-300"
+				contenteditable="true"
+				bind:textContent={description}
+				on:blur={updateProjectDescription}
+			>
+				{$currentProject.description}
+			</p>
 		{/if}
-	</div>
-	{#if inEditMode}
-		<section class="mt-md">
+
+		{#if $userTeams?.length > 0}
+			<TransferProjectToTeam
+				{teamName}
+				projectId={$currentProject.id}
+				team={$currentProject.team}
+			/>
+		{/if}
+
+		<section>
 			<header>
-				<h2 class="text-md font-semibold text-grey-900 dark:text-grey-100">Cover Properties</h2>
+				<h2 class="text-md font-semibold text-grey-700 dark:text-grey-200">Project Appearance</h2>
 			</header>
-
-			<input type="file" class="hidden" bind:this={coverInputElement} bind:files={newCoverFile} />
-
-			{#if !newCoverURL}
-				<button
-					class="button--secondary flex w-full items-center justify-center gap-md"
-					on:click={() => coverInputElement.click()}
-				>
-					<Image className="stroke-grey-70 dark:text-grey-1000 w-6 h-6" />
-					Set a project cover
-				</button>
-			{:else if newCoverURL}
-				<button
-					class="button--primary flex w-full items-center justify-center gap-md"
-					on:click={() => coverInputElement.click()}
-				>
-					<Image className="stroke-grey-200 w-6 h-6" />
-					Update project cover
-				</button>
-				<button
-					class="button--secondary mt-sm flex w-full items-center justify-center gap-md"
-					on:click={handleRemoveCover}
-				>
-					<Trash className="stroke-grey-700 dark:stroke-grey-100 w-6 h-6" />
-					Remove cover
-				</button>
-			{/if}
+			<FileInput
+				bind:bannerURL
+				bind:newBanner
+				uploadBanner={updateProjectBanner}
+				postRemoveBannnerHandle={removeBanner}
+			/>
 		</section>
 
-		<footer class="mx-auto mt-xl flex w-1/2 items-center justify-around gap-md">
-			<button class="button--secondary" on:click={() => (inEditMode = false)}>Cancel</button>
-			<button class="button--primary" on:click={handleUpdateProject}>Save</button>
-		</footer>
-	{/if}
+		<InvitedTeamMembers
+			invited_people={$currentProject.invited_people}
+			projectId={$currentProject.id}
+		/>
+	</div>
 </dialog>
 
 <ManageInvited bind:shown={$showManageInvitedPrompt} />
