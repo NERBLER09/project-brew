93905fbbe6bce6992c925fcb7f343271cfc2e7bc
Finnegan Beaudin <noah.beaudin1@gmail.com>
2025-07-23 11:53:21 -0600

    Add placeholder label for adding tags to a project

--- src/lib/components/prompts/about/TagInput.svelte
+++ src/lib/components/prompts/about/TagInput.svelte
@@ -26,11 +26,12 @@
 	};
 </script>
 
-<div class="flex">
+<div class="flex items-center">
 	<div class="flex gap-sm">
 		{#each newTags as tag}
 			<div class="flex w-fit items-center rounded bg-grey-200 px-3 py-2 dark:bg-grey-800">
 				<span class="font-medium text-grey-700 dark:text-grey-200">{tag}</span>
+
 				<button on:click={() => handleRemoveTag(tag)}>
 					<Trash className="w-6 h-6 stroke-grey-700 dark:stroke-grey-200" />
 					<span class="sr-only">Remove {tag} tag</span>
@@ -40,8 +41,12 @@
 	</div>
 
 	{#if addNewTag}
-		<form on:submit={handleCreateNewTag} class="ml-auto flex items-center gap-sm">
+		<form
+			on:submit={handleCreateNewTag}
+			class="{newTags ? '' : 'ml-auto'} flex items-center gap-sm"
+		>
 			<input
+				id="tag"
 				type="text"
 				class="input--text w-36"
 				placeholder="Tag name"
@@ -58,9 +63,9 @@
 			</button>
 		</form>
 	{:else}
-		<button type="button" on:click={() => (addNewTag = true)} class="ml-auto">
+		<span class="input--label h-fit"> Add a tag to this project</span>
+		<button type="button" on:click={() => (addNewTag = true)}>
 			<PlusNew className="h-8 w-8 stroke-grey-700 dark:stroke-grey-200" />
-			<span class="sr-only">Add new tag</span>
 		</button>
 	{/if}
 </div>
