c7bd5dab3ee59acfbec7928137bada89f12098d5
Finnegan Beaudin <noah.beaudin1@gmail.com>
2025-07-25 13:26:27 -0600

    Added a show/hide password button to login/signup page

--- src/routes/(landing-page)/login/+page.svelte
+++ src/routes/(landing-page)/login/+page.svelte
@@ -4,8 +4,12 @@
 
 	import Google from '$lib/assets/Logo/Google.svelte';
 	import type { ActionData } from './$types';
+	import Lock from '$lib/assets/Landing-Page/Lock.svelte';
+	import Unlock from '$lib/assets/Unlock.svelte';
 
 	export let form: ActionData;
+
+	let showPassword = false;
 </script>
 
 <svelte:head>
@@ -14,27 +18,32 @@
 </svelte:head>
 
 <section
-	class="absolute top-0 mx-6 grid h-screen place-content-center overflow-hidden text-center md:w-screen"
+	class="absolute top-0 grid h-screen w-screen place-content-center overflow-hidden text-center"
 >
 	<header>
 		<h1 class="text-xl font-semibold text-grey-800 sm:text-2xl">Log in</h1>
 	</header>
 	<form method="POST" class="flex flex-col items-center gap-[0.625rem]" use:enhance>
-		<input
-			type="email"
-			class="input--text w-full md:w-72"
-			placeholder="Email"
-			name="email"
-			required
-		/>
+		<input type="email" class="input--text w-full" placeholder="Email" name="email" required />
 
-		<input
-			type="password"
-			class="input--text w-full md:w-72"
-			placeholder="Password"
-			name="password"
-			required
-		/>
+		<div class="input--text flex max-h-[40px] w-full items-center">
+			<input
+				type={showPassword ? 'text' : 'password'}
+				class="input--text m-0 w-full border-none bg-grey-200 p-0 md:w-72"
+				placeholder="Password"
+				name="password"
+				required
+			/>
+			<button on:click={() => (showPassword = !showPassword)}>
+				{#if !showPassword}
+					<Lock className="stroke-grey-700 w-6 h-6 ml-auto" />
+					<span class="sr-only">Show password</span>
+				{:else}
+					<Unlock className="stroke-grey-700 w-6 h-6 ml-auto" />
+					<span class="sr-only">Hide password</span>
+				{/if}
+			</button>
+		</div>
 
 		{#if form?.fail}
 			<p class="my-sm font-medium text-red-600">{form?.message}</p>
--- src/routes/(landing-page)/sign-up/+page.svelte
+++ src/routes/(landing-page)/sign-up/+page.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { handleGoogleAuth, handleUserSignUp } from '$lib/api/auth';
-
 	import Google from '$lib/assets/Logo/Google.svelte';
+	import Lock from '$lib/assets/Landing-Page/Lock.svelte';
+	import Unlock from '$lib/assets/Unlock.svelte';
 
 	let email = '';
 	let password = '';
@@ -9,6 +10,8 @@
 	const handleSignUp = () => {
 		handleUserSignUp(email, password);
 	};
+
+	let showPassword = false;
 </script>
 
 <svelte:head>
@@ -20,7 +23,7 @@
 </svelte:head>
 
 <section
-	class="absolute top-0 mx-6 grid h-screen place-content-center overflow-hidden text-center md:w-screen"
+	class="absolute top-0 mx-6 grid h-screen w-screen place-content-center overflow-hidden text-center"
 >
 	<header>
 		<h1 class="text-xl font-semibold text-grey-800 sm:text-2xl">Sign Up</h1>
@@ -28,18 +31,30 @@
 	<form on:submit|preventDefault={handleSignUp} class="flex flex-col items-center gap-[0.625rem]">
 		<input
 			type="email"
-			class="input--text w-full md:w-72"
+			class="input--text w-full"
 			placeholder="Email"
 			required
 			bind:value={email}
 		/>
-		<input
-			type="password"
-			class="input--text w-full md:w-72"
-			placeholder="Password"
-			required
-			bind:value={password}
-		/>
+		<div class="input--text flex max-h-[40px] w-full items-center">
+			<input
+				type={showPassword ? 'text' : 'password'}
+				class="input--text m-0 w-full border-none bg-grey-200 p-0 md:w-72"
+				placeholder="Password"
+				name="password"
+				required
+			/>
+			<button on:click={() => (showPassword = !showPassword)}>
+				{#if !showPassword}
+					<Lock className="stroke-grey-700 w-6 h-6 ml-auto" />
+					<span class="sr-only">Show password</span>
+				{:else}
+					<Unlock className="stroke-grey-700 w-6 h-6 ml-auto" />
+					<span class="sr-only">Hide password</span>
+				{/if}
+			</button>
+		</div>
+
 		<button class="button--primary w-full" type="submit">Create account</button>
 		<span>or</span>
 		<button
